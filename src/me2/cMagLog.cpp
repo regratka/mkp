b63d4e099d6bc5ebe0864c18ee7d2708e56961f3
Aszman <lukaszstarzyk1@gmail.com>
2026-01-31 19:51:26 +0100

    init

---
+++ src/me2/cMagLog.cpp
@@ -0,0 +1,152 @@
+#include "cMagLog.h"
+
+#include <stdio.h>
+#include <cstring>
+#include <stdlib.h>
+#include <windows.h>
+
+cMagLog::cMagLog(){
+    logEnabled = false;
+    logFilename = "log.txt";
+    ReadSettings("data\\Ustawienia.cfg");
+}
+
+void cMagLog::DeleteLog() {
+    DeleteFileA(logFilename);
+    DeleteFileA("loadtime.txt");
+    DeleteFileA("crash.txt");
+    DeleteFileA("TexCrash.txt");
+    DeleteFileA("data.txt");
+    DeleteFileA("clientserver.txt");
+    DeleteFileA("Textures.txt");
+    DeleteFileA("Dummy.txt");
+}
+
+void cMagLog::DataLog(char const * format, ...) {
+    if (logEnabled == false) {
+        return;
+    }
+    char buffer[512];
+    va_list args;
+    va_start(args, format);
+    _snprintf(buffer, 0x200, format, args);
+    FILE* dataFile = fopen("data.txt", "a");
+    fprintf(dataFile, "%s", buffer);
+    fclose(dataFile);
+}
+
+void cMagLog::CrashLog(char const * format, ...) {
+    if (logEnabled == false) {
+        return;
+    }
+    char buffer[512];
+    va_list args;
+    va_start(args, format);
+    _snprintf(buffer, 0x200, format, args);
+    FILE* dataFile = fopen("crash.txt", "a");
+    fprintf(dataFile, "%s", buffer);
+    fclose(dataFile);
+    DebugLog(buffer);
+}
+
+void cMagLog::TextureErrorLog(char const * format, ...) {
+    if (logEnabled == false) {
+        return;
+    }
+    char buffer[512];
+    va_list args;
+    va_start(args, format);
+    _snprintf(buffer, 0x200, format, args);
+    FILE* dataFile = fopen("TextCrash.txt", "a");
+    fprintf(dataFile, "%s", buffer);
+    fclose(dataFile);
+    DebugLog(buffer);
+}
+
+void cMagLog::TexturesLog(char const * format, ...) {
+    if (logEnabled == false) {
+        return;
+    }
+    char buffer[512];
+    va_list args;
+    va_start(args, format);
+    _snprintf(buffer, 0x200, format, args);
+    FILE* dataFile = fopen("Textures.txt", "a");
+    fprintf(dataFile, "%s", buffer);
+    fclose(dataFile);
+    DebugLog(buffer);
+}
+
+void cMagLog::FileLog(char const * format, ...) {
+    if (logEnabled == false) {
+        return;
+    }
+    char buffer[512];
+    va_list args;
+    va_start(args, format);
+    _snprintf(buffer, 0x200, format, args);
+    FILE* dataFile = fopen(logFilename, "a");
+    fprintf(dataFile, "%s", buffer);
+    fclose(dataFile);
+    DebugLog(buffer);
+}
+
+void cMagLog::DummyLog(char const * format, ...) {
+    if (logEnabled == false) {
+        return;
+    }
+    char buffer[512];
+    va_list args;
+    va_start(args, format);
+    _snprintf(buffer, 0x200, format, args);
+    FILE* dataFile = fopen("Dummy.txt", "a");
+    fprintf(dataFile, "%s", buffer);
+    fclose(dataFile);
+    DebugLog(buffer);
+}
+
+void cMagLog::ClientServerLog(char const * format, ...) {
+    if (logEnabled == false) {
+        return;
+    }
+    char buffer[512];
+    va_list args;
+    va_start(args, format);
+    _snprintf(buffer, 0x200, format, args);
+    FILE* dataFile = fopen("clientserver.txt", "a");
+    fprintf(dataFile, "%s", buffer);
+    fclose(dataFile);
+    DebugLog(buffer);
+}
+
+void cMagLog::DebugLog(char const * format, ...) {
+    if (logEnabled == false) {
+        return;
+    }    
+    char buffer[512];
+    va_list args;
+    va_start(args, format);
+    _snprintf(buffer,0x200,format,args);
+    OutputDebugStringA(buffer);
+}
+
+
+void cMagLog::ReadSettings(char* settingsPath) {
+    FILE* settingsFile = fopen(settingsPath, "r");
+    if (settingsFile == NULL) {
+        return;
+    }
+    char buffer[101] = {0};
+    while (!feof(settingsFile)) {
+        fgets(buffer, 99, settingsFile);
+        char* settingName = strtok(buffer," :,");
+        bool cmpRes = strcmp(settingName, "Log");
+        if (cmpRes == 0) {
+            char* settingVal = strtok(NULL," :");
+            if (settingVal != NULL) {
+                logEnabled = atoi(settingVal);
+            }
+        }
+    }
+    fclose(settingsFile);
+}
---
+++ src/me2/cMagLog.h
@@ -0,0 +1,19 @@
+class cMagLog {
+public:
+    cMagLog();
+    void DeleteLog();
+    void DataLog(char const * format, ...);
+    void CrashLog(char const * format, ...);
+    void TextureErrorLog(char const * format, ...);
+    void ClientServerLog(char const * format, ...);
+    void TexturesLog(char const * format, ...);
+    void FileLog(char const * format, ...);
+    void DummyLog(char const * format, ...);
+    void DebugLog(char const * format, ...);
+
+    void ReadSettings(char* settingsPath);
+
+private:
+    bool logEnabled;
+    char* logFilename;
+};
