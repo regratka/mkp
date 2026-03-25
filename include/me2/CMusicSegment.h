86e85928d66c12d8f4ae35adedceeb12784d5f53
Aszman <lukaszstarzyk1@gmail.com>
2026-03-25 17:59:37 +0100

    matching CMusicSegment

--- include/me2/CMusicSegment.h
+++ include/me2/CMusicSegment.h
@@ -10,14 +10,14 @@ class __declspec(dllexport) CMusicSegment {
 public:
 	/* 1009B150 */ CMusicSegment(IDirectMusicPerformance8* param_1, IDirectMusicLoader8* param_2, IDirectMusicSegment8* param_3);
 	/* 1009B1C0 */ virtual ~CMusicSegment();
-	/* 1009B230 */ long Play(ulong param_1, uchar param_2);
-	/* 1009B290 */ long Download(IDirectMusicAudioPath* param_1);
-	/* 1009B2E0 */ long Unload(uchar param_1);
-	/* 1009B330 */ long SetVolume(int param_1, IDirectMusicAudioPath* param_2);
+	/* 1009B230 */ HRESULT Play(ulong param_1, IDirectMusicAudioPath8* param_2);
+	/* 1009B290 */ HRESULT Download(IDirectMusicAudioPath8* param_1);
+	/* 1009B2E0 */ HRESULT Unload(IDirectMusicAudioPath8* param_1);
+	/* 1009B330 */ HRESULT SetVolume(int param_1, IDirectMusicAudioPath8* param_2);
 	/* 1009B3A0 */ int IsPlaying();
-	/* 1009B3D0 */ long Stop(ulong param_1);
-	/* 1009B400 */ long SetRepeats(ulong param_1);
-	/* 1009B420 */ long GetStyle(IDirectMusicStyle8** param_1, ulong param_2);
+	/* 1009B3D0 */ HRESULT Stop(ulong param_1);
+	/* 1009B400 */ HRESULT SetRepeats(ulong param_1);
+	/* 1009B420 */ HRESULT GetStyle(IDirectMusicStyle8** param_1, ulong param_2);
 
 private:
 	/* 0x04 */ IDirectMusicSegment8* directMusicSegment;
--- src/me2/CMusicSegment.cpp
+++ src/me2/CMusicSegment.cpp
@@ -50,42 +50,112 @@ CMusicSegment::~CMusicSegment() {
 }
 
 /* 1009B230-1009B289 00059	*/
-long CMusicSegment::Play(ulong param_1, uchar param_2) {
-	return 0;
+HRESULT CMusicSegment::Play(ulong param_1, IDirectMusicAudioPath8* param_2) {
+	if (directMusicSegment == NULL || directMusicPerformance == NULL) { 
+		return CO_E_NOTINITIALIZED;
+	}
+
+	if (!downloaded) {
+		return E_FAIL;
+	}
+
+	if (param_2 == NULL && directMusicAudioPath != NULL) {
+		param_2 = directMusicAudioPath;
+	}
+
+	return directMusicPerformance->PlaySegmentEx(directMusicSegment, NULL, NULL, param_1, 0, NULL, NULL, param_2);
 }
 
 /* 1009B290-1009B2D4 00044	*/
-long CMusicSegment::Download(IDirectMusicAudioPath* param_1) {
-	return 0;
+HRESULT CMusicSegment::Download(IDirectMusicAudioPath8* param_1) {
+	if (directMusicSegment == NULL) { 
+		return CO_E_NOTINITIALIZED;
+	}
+	if (param_1 == NULL) {
+		param_1 = directMusicAudioPath;
+	}
+	HRESULT result = param_1 == NULL 
+		? directMusicSegment->Download(directMusicPerformance) 
+		: directMusicSegment->Download(param_1);
+
+	if (result >= S_OK) {
+		downloaded = TRUE;
+	}
+	return result;
 }
 
 /* 1009B2E0-1009B324 00044	*/
-long CMusicSegment::Unload(uchar param_1) {
-	return 0;
+HRESULT CMusicSegment::Unload(IDirectMusicAudioPath8* param_1) {
+	if (directMusicSegment == NULL) { 
+		return CO_E_NOTINITIALIZED;
+	}
+	if (param_1 == NULL) {
+		param_1 = directMusicAudioPath;
+	}
+	HRESULT result = param_1 == NULL 
+		? directMusicSegment->Unload(directMusicPerformance) 
+		: directMusicSegment->Unload(param_1);
+		
+	if (result >= S_OK) {
+		downloaded = FALSE;
+	}
+	return result;
 }
 
 /* 1009B330-1009B39F 0006F	*/
-long CMusicSegment::SetVolume(int param_1, IDirectMusicAudioPath* param_2) {
-	return 0;
+HRESULT CMusicSegment::SetVolume(int param_1, IDirectMusicAudioPath8* param_2) {
+	if (directMusicSegment == NULL || directMusicPerformance == NULL) { 
+		return CO_E_NOTINITIALIZED;
+	}
+
+	if (!downloaded) {
+		return E_FAIL;
+	}
+
+	if (param_2 == NULL && directMusicAudioPath != NULL) {
+		param_2 = directMusicAudioPath;
+	}
+
+	int volume;
+	if (param_1 <= 0) {
+		volume = -9600;
+	} else {
+		volume = (param_1-100) * 25.0;
+	}
+	if (param_2 != NULL) {
+		return param_2->SetVolume(volume, 0);
+	}
+	return volume;
 }
 
 /* 1009B3A0-1009B3C3 00023	*/
 int CMusicSegment::IsPlaying() {
-	return 0;
+	if (directMusicSegment != NULL && directMusicPerformance != NULL) {
+		return directMusicPerformance->IsPlaying(directMusicSegment, NULL) == S_OK;
+	}
+	return CO_E_NOTINITIALIZED;
 }
 
 /* 1009B3D0-1009B3FB 0002B	*/
-long CMusicSegment::Stop(ulong param_1) {
-	return 0;
+HRESULT CMusicSegment::Stop(ulong param_1) {
+	if (directMusicSegment != NULL && directMusicPerformance != NULL) {
+		return directMusicPerformance->Stop(directMusicSegment, NULL, 0, param_1);
+	}
+	return CO_E_NOTINITIALIZED;
 }
 
 /* 1009B400-1009B41D 0001D	*/
-long CMusicSegment::SetRepeats(ulong param_1) {
-	return 0;
+HRESULT CMusicSegment::SetRepeats(ulong param_1) {
+	if (directMusicSegment == NULL) {
+		return CO_E_NOTINITIALIZED;
+	}
+	
+	return directMusicSegment->SetRepeats(param_1);
 }
 
+
 /* 1009B420-1009B441 00021	*/
-long CMusicSegment::GetStyle(IDirectMusicStyle8** param_1, ulong param_2) {
-	return 0;
+HRESULT CMusicSegment::GetStyle(IDirectMusicStyle8** param_1, ulong param_2) {
+	return directMusicSegment->GetParam(GUID_IDirectMusicStyle, 0xffffffff, param_2, 0, NULL, param_1);
 }
 
