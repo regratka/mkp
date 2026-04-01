7e44bec55d750a0bc8d97a3f3e504e5889a17af0
Aszman <lukaszstarzyk1@gmail.com>
2026-04-01 19:18:33 +0200

    CJVMData progress

--- src/me2/CJVMData.cpp
+++ src/me2/CJVMData.cpp
@@ -5,8 +5,8 @@ CJVMData::CJVMData(JNIEnv* param_1) {
 	jniEnv = param_1;
 	jniEnv->GetJavaVM(&javaVM);
 
-	FUN_1009e980("game.ini");
-	FUN_1009cd00();
+	FUN1009e980("game.ini");
+	FUN1009cd00();
 }
 
 /* 1009CA80-1009CC03 00183	*/
@@ -35,20 +35,87 @@ jobject CJVMData::NewObject(char* param_1) {
 }
 
 /* 1009CD00-1009CD69 00069	*/
-void CJVMData::FUN_1009cd00() {
+void CJVMData::FUN1009cd00() {
 	WIN32_FIND_DATA findData;
 	HANDLE handle = FindFirstFile("java\\*.class", &findData);
 	if (handle == INVALID_HANDLE_VALUE) {
 		exit(0);
 	}
-	FUN_1009cd70(findData.cFileName);
+	FUN1009cd70(findData.cFileName);
 	while(FindNextFile(handle, &findData)) {
-		FUN_1009cd70(findData.cFileName);
+		FUN1009cd70(findData.cFileName);
 	}
 }
 
 /* 1009CD70-1009DF6A 011FA	*/
-void CJVMData::FUN_1009cd70(char* param_1) {
+void CJVMData::FUN1009cd70(char* param_1) {
+	char* className = strtok(param_1, ".");
+	jclass clazz = jniEnv->FindClass(className);
+	if (clazz == NULL) {
+		char buffer[300];
+		sprintf(buffer, "Can't find %s class\n", param_1);
+		MessageBox(NULL, buffer, "", 0);
+		exit(1);
+	}
+
+	if (strcmp("GameObject", className) == 0) {
+		gameObjectClass  = clazz;
+		thisID = jniEnv->GetFieldID(clazz, "ThisID", "I");
+		thisID = jniEnv->GetFieldID(clazz, "CurrentState", "LState;");
+	}
+
+	if (strcmp(className, "MeshObject") == 0) {
+		meshObjectClass = clazz;
+	}
+
+	if (strcmp(className, "Vector")) {
+		fxID = jniEnv->GetFieldID(clazz, "fX", "F");
+		fxID = jniEnv->GetFieldID(clazz, "fY", "F");
+		fzID = jniEnv->GetFieldID(clazz, "fZ", "F");
+		vectorInitMethodID = jniEnv->GetMethodID(clazz, "<init>", "()V");
+		vectorInitMethodID2 = jniEnv->GetMethodID(clazz, "<init>", "(FFF)V");
+		vectorClass = clazz;
+	}
+
+	if (strcmp("Matrix", className)) {
+		for (int index1; index1 < 4; index1++) {
+			for (int index2; index2 < 4; index2++) {
+				char matrixFieldBuffer[8];
+				sprintf(matrixFieldBuffer, "_%d%d", index1, index2);
+				matrixFieldsIDs[index1 * 4 + index2] = jniEnv->GetFieldID(clazz, matrixFieldBuffer, "F");
+			}
+		}
+		matrixInitMethodID = jniEnv->GetMethodID(clazz, "<init>", "()V");
+		matrixClass = clazz;
+	}
+
+	if (strcmp("PatrolPoint", className)) {
+		patrolPointPosFieldID = jniEnv->GetFieldID(clazz, "vPos", "LVector;");
+		patrolPointRadiusFieldID = jniEnv->GetFieldID(clazz, "fRadius", "F");
+		patrolPointFloatFieldID = jniEnv->GetFieldID(clazz, "fFloat", "F");
+		patrolPointDataFieldID = jniEnv->GetFieldID(clazz, "sData", "Ljava/lang/String;");
+		patrolPointInitMethodID = jniEnv->GetMethodID(clazz, "<init>", "()V");
+		patrolPointClass = clazz;
+	}
+	
+	if (strcmp("Collision", className)) {
+		collisionTexNameFieldID = jniEnv->GetFieldID(clazz, "sTexName", "Ljava/lang/String;");
+		collisionColliderFieldID = jniEnv->GetFieldID(clazz, "cCollider", "LMeshObject;");
+		collisionExcludeFieldID = jniEnv->GetFieldID(clazz, "cExclude", "LMeshObject;");
+		collisionOrygFieldID = jniEnv->GetFieldID(clazz, "vOryg", "LVector;");
+		collisionDeltaFieldID = jniEnv->GetFieldID(clazz, "vDelta", "LVector;");
+		collisionNormalFieldID = jniEnv->GetFieldID(clazz, "vNormal", "LVector;");
+		collisionVertexFieldID = jniEnv->GetFieldID(clazz, "vVertex", "[LVector;");
+		collisionTexIDFieldID = jniEnv->GetFieldID(clazz, "byTexID", "B");
+		collisionDistFieldID = jniEnv->GetFieldID(clazz, "fDist", "F");
+		collisionInitMethodID = jniEnv->GetMethodID(clazz, "<init>", "()V");
+		collisionClass = clazz;
+	}
+
+	if (strcmp("Rect", className)) {
+		
+	}
+
 	return;
 }
 
@@ -58,12 +125,12 @@ cMagGameObject* CJVMData::CreateObject(char* param_1) {
 }
 
 /* 1009E960-1009E978 00018	*/
-uchar CJVMData::FUN_1009e960(uint param_1) {
+uchar CJVMData::FUN1009e960(uint param_1) {
 	return 0;
 }
 
 /* 1009E980-1009E9E8 00068	*/
-void CJVMData::FUN_1009e980(char* param_1) {
+void CJVMData::FUN1009e980(char* param_1) {
 
 	char buffer[299];
 	GetCurrentDirectory(sizeof(buffer), buffer);
@@ -73,11 +140,11 @@ void CJVMData::FUN_1009e980(char* param_1) {
 
 
 /* 1009E9F0-1009EA67 00077	*/
-void CJVMData::FUN_1009e9f0() {
+void CJVMData::FUN1009e9f0() {
 }
 
 /* 1009EC60-1009EC74 00014	*/
-uchar CJVMData::FUN_1009ec60(uint param_1) {
+uchar CJVMData::FUN1009ec60(uint param_1) {
 	return 0;
 }
 
