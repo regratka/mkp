428242201f2ad37002eeafb25d7175545bff4c64
Aszman <lukaszstarzyk1@gmail.com>
2026-05-30 00:32:30 +0200

    AIControlPoint matching

--- include/mkp/AIControlPoint.h
+++ include/mkp/AIControlPoint.h
@@ -3,14 +3,19 @@
 
 #include <globals.h>
 
-class AIControlPoint {
+#include "_AI.h"
+
+class AIControlPoint : public _AI {
 public:
 	/* 43CE20 */ AIControlPoint();
-	/* 43CE40 */ uchar scalar_destructor(uchar param_1);
 	/* 43CE60 */ ~AIControlPoint();
 	/* 43CE70 */ void* EXP();
 	/* 43CEE0 */ bool FUN0043cee0();
 	/* 43CEF0 */ void FUN0043cef0(bool param_1);
+private:
+	/* 0x2658 */ bool unk_2658;
 };
+
+STATIC_ASSERT(sizeof(AIControlPoint) == 0x2660);
 
 #endif
--- src/mkp/AIControlPoint.cpp
+++ src/mkp/AIControlPoint.cpp
@@ -2,11 +2,7 @@
 
 /* 43CE20-43CE39 00019	*/
 AIControlPoint::AIControlPoint() {
-}
-
-/* 43CE40-43CE5E 0001E	*/
-uchar AIControlPoint::scalar_destructor(uchar param_1) {
-	return 0;
+	unk_2658 = true;
 }
 
 /* 43CE60-43CE6B 0000B	*/
@@ -15,15 +11,15 @@ AIControlPoint::~AIControlPoint() {
 
 /* 43CE70-43CECA 0005A	*/
 void* AIControlPoint::EXP() {
-	return 0;
+	return new AIControlPoint();
 }
 
 /* 43CEE0-43CEE7 00007	*/
 bool AIControlPoint::FUN0043cee0() {
-	return 0;
+	return unk_2658;
 }
 
 /* 43CEF0-43CEFD 0000D	*/
 void AIControlPoint::FUN0043cef0(bool param_1) {
+	unk_2658 = param_1;
 }
-
