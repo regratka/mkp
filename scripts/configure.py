dadb0a9aefa8dea737d9c0467b705a39754d7e84
Łukasz Starzyk <72223520+Aszman@users.noreply.github.com>
2026-05-30 16:54:26 +0200

    [mkp] AIControlPoint matching (#5)
    * AIControlPoint matching
    
    * updated report.json
    
    * added accidentialy removed function

--- scripts/configure.py
+++ scripts/configure.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import argparse
 from generate_sources import generateSources
 from generate_ninja import generateNinja
 from generate_objdiff import generateObjdiffConfig
@@ -9,15 +10,22 @@ import os
 
 
 def configureProject(base_dir: Path):
+    parser = argparse.ArgumentParser(
+        description="Configure decompilation project",
+    )
+    parser.add_argument("--skipexport", action='store_true')
+    args = parser.parse_args()
+
     os.chdir(base_dir)
     engineUnit = DecompUnit("Engine", "me2", "ME2.dll")
     gameUnit = DecompUnit("Game code", "mkp", "gratka3d.exe")
-    configure([engineUnit, gameUnit])
+    configure([engineUnit, gameUnit], args.skipexport)
 
-def configure(decompUnits: list[DecompUnit]):
-    for unit in decompUnits:
-        exportObjs(unit)
-        generateSources(unit)
+def configure(decompUnits: list[DecompUnit], skipExport: bool):
+    if not skipExport:
+        for unit in decompUnits:
+            exportObjs(unit)
+            generateSources(unit)
         
     generateNinja(decompUnits, Path("."))
     generateObjdiffConfig(decompUnits, Path("./build/orig"), Path("./build/src"), Path("."))
