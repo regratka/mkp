6ee473894d6edbcc8422b8c212887eb5ae0a415d
Aszman <lukaszstarzyk1@gmail.com>
2026-06-07 17:58:15 +0200

    first decomp CFreeCamera progress

--- include/me2/CFreeCamera.h
+++ include/me2/CFreeCamera.h
@@ -5,23 +5,26 @@
 
 #include "CCamera.h"
 
-class CFreeCamera : public CCamera {
+class DLLEXPORT CFreeCamera : public CCamera {
 public:
-	/* 1000BFF0 */ CFreeCamera(CFreeCamera* param_1);
-	/* 1000C120 */ CFreeCamera* operator=(CFreeCamera* param_1);
-	/* 1000C240 */ CFreeCamera* scalar_destructor(uchar param_1);
 	/* 10061C20 */ CFreeCamera();
-	/* 10061CE0 */ ~CFreeCamera();
+	/* 10061CE0 */ virtual ~CFreeCamera();
 	/* 10061CF0 */ void OnInputKey(uchar* param_1);
 	/* 10061D10 */ void OnInputMouse(float param_1, float param_2, bool param_3, bool param_4);
 	/* 10061F80 */ double GetUpdate(int param_1);
-	/* 10062050 */ void OnActivateView();
+	/* 10062050 */ virtual void OnActivateView();
 	/* 100620C0 */ void SetSpeedValue(float param_1);
 	/* 100620D0 */ bool CheckWindowActivation();
 	/* 10062120 */ void ReadSettings(char* param_1);
 
 private:
-	/* 0xdec */ uchar field_0xdec[0xe18-0xdec];
+	/* 0xdf0 */ double unk_df0;
+	/* 0xdf8 */ double unk_df8;
+	/* 0xe00 */ double unk_e00;
+	/* 0xe08 */ HWND activeWindow;
+	/* 0xe0c */ float freeCameraSpeed;
+	/* 0xe10 */ bool unk_e10;
+	/* 0xe14 */ float freeCameraAngSpeed;
 };
 
 STATIC_ASSERT(sizeof(CFreeCamera) == 0xe18);
