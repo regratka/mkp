62bab8f40b8dc2370b20153edddd688f616345d3
Łukasz Starzyk <72223520+Aszman@users.noreply.github.com>
2026-06-15 18:45:56 +0200

    matched CBalon (#11)

--- include/mkp/CBalon.h
+++ include/mkp/CBalon.h
@@ -2,17 +2,28 @@
 #define _C_BALON
 
 #include <globals.h>
+#include <vector>
 
-class CBalon {
+#include "cMagMeshObject.h"
+
+
+class CBalon : public cMagMeshObject {
 public:
 	/* 4400B0 */ CBalon();
-	/* 440140 */ uchar scalar_destructor(uchar param_1);
 	/* 440160 */ ~CBalon();
 	/* 4401A0 */ void* EXP();
 	/* 440200 */ void OnAction();
 	/* 440220 */ void OnFrame();
-	/* 440300 */ void OnDestinationPos();
-	/* 440350 */ uchar OnLoadChunk(int param_1, uint param_2, uint param_3, void* param_4);
+	/* 440300 */ void OnDestinationPos(D3DXVECTOR3 param_1);
+	/* 440350 */ void OnLoadChunk(_ED_CHUNK param_1, std::ifstream& param_2);
+
+private:
+	/* 0x2658 */ float* pointsLoadBuffer;
+	/* 0x265c */ std::vector<D3DXVECTOR3> routePoints;
+	/* 0x266c */ int visitedPoints;
+	/* 0x2670 */ int routePointsAmount;
 };
+
+STATIC_ASSERT(sizeof(CBalon) == 0x2678);
 
 #endif
--- src/mkp/CBalon.cpp
+++ src/mkp/CBalon.cpp
@@ -2,11 +2,9 @@
 
 /* 4400B0-440139 00089	*/
 CBalon::CBalon() {
-}
-
-/* 440140-44015E 0001E	*/
-uchar CBalon::scalar_destructor(uchar param_1) {
-	return 0;
+	pointsLoadBuffer = NULL;
+	visitedPoints = 0;
+	routePoints.clear();
 }
 
 /* 440160-44019C 0003C	*/
@@ -15,23 +13,54 @@ CBalon::~CBalon() {
 
 /* 4401A0-4401FA 0005A	*/
 void* CBalon::EXP() {
-	return 0;
+	return new CBalon();
 }
 
 /* 440200-44021F 0001F	*/
 void CBalon::OnAction() {
+	EnableCallHandler("OnFrame");
+	SetSpeedValue(200.0f);
 }
 
 /* 440220-4402F6 000D6	*/
 void CBalon::OnFrame() {
+	if (routePointsAmount <= 0) {
+		return;
+	}
+
+	D3DXVECTOR3 destPos(routePoints[visitedPoints].x, routePoints[visitedPoints].y, routePoints[visitedPoints].z);
+	destPos.y = GetPosition().y;
+	SetDestinationPos(destPos, 200.0f);
+	D3DXVECTOR3 auStack18 = GetPosition();
+	D3DXVECTOR3 local_28 = destPos - auStack18;
+	D3DXVec3Normalize(&local_28, &local_28);
+	SetDestinationDir(local_28, 2);
 }
 
 /* 440300-440343 00043	*/
-void CBalon::OnDestinationPos() {
+void CBalon::OnDestinationPos(D3DXVECTOR3 param_1) {
+	visitedPoints++;
+	if (visitedPoints >= routePointsAmount) {
+		visitedPoints = 0;
+		DisableCallHandler("OnFrame");
+		SetSpeedValue(0.0f);
+	}
 }
 
 /* 440350-44060A 002BA	*/
-uchar CBalon::OnLoadChunk(int param_1, uint param_2, uint param_3, void* param_4) {
-	return 0;
+void CBalon::OnLoadChunk(_ED_CHUNK param_1, std::ifstream& param_2) {
+	if (param_1.chunkType != 32) {
+		return;
+	}
+	param_2.read((char*) &routePointsAmount, 4);
+	pointsLoadBuffer = new float[routePointsAmount*3];
+	
+	param_2.read((char*) pointsLoadBuffer, routePointsAmount*sizeof(D3DXVECTOR3));
+	for (int index = 0; index < routePointsAmount; index++) {
+		routePoints.push_back(D3DXVECTOR3(pointsLoadBuffer[index*3],pointsLoadBuffer[index*3+1], pointsLoadBuffer[index*3+2]));
+	}
+
+	delete[] pointsLoadBuffer;
+	pointsLoadBuffer = NULL;
 }
 
