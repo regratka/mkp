6c64959681a606e92a0a6d8883025ac021f46706
Aszman <lukaszstarzyk1@gmail.com>
2026-06-27 20:31:54 +0200

    matching CKrysztal

--- include/mkp/CKrysztal.h
+++ include/mkp/CKrysztal.h
@@ -3,14 +3,32 @@
 
 #include <globals.h>
 
-class CKrysztal {
+#include "_PowerUp.h"
+
+struct CKrysztalSaveData{
+	/* 0x00 */ BOOL unk_00;
+	/* 0x04 */ BOOL unk_04;
+	/* 0x08 */ int unk_08;
+};
+
+class CKrysztal : public _PowerUp {
 public:
+	/* 419BA0 */ void OnActivateLevel();
 	/* 432DB0 */ CKrysztal();
-	/* 432DD0 */ CKrysztal* scalar_destructor(uchar param_1);
 	/* 432DF0 */ ~CKrysztal();
 	/* 432E00 */ void* EXP();
-	/* 432F70 */ uchar OnCollisionObject(int* param_1);
-	/* 433090 */ uchar Load(void* param_1, int param_2, int param_3, uint param_4);
+	/* 432E60 */ void OnActivate();
+	/* 432E70 */ void OnActivateInternal();
+	/* 432F20 */ void FUN00432f20();
+	/* 432F70 */ void OnCollisionObject(cMagMeshObject* param_1);
+	/* 432FD0 */ void Save(std::ostream& param_1);
+	/* 433090 */ void Load(std::ifstream& param_1, _ED_CHUNK param_2);
+
+private:
+	/* 0x2658 */ bool unk_2658;
+	/* 0x2659 */ bool unk_2659;
 };
+
+STATIC_ASSERT(sizeof(CKrysztal) == 0x2660);
 
 #endif
--- src/mkp/CKrysztal.cpp
+++ src/mkp/CKrysztal.cpp
@@ -1,12 +1,16 @@
 #include "CKrysztal.h"
 
-/* 432DB0-432DD0 00020	*/
-CKrysztal::CKrysztal() {
+#include "GameSDK.h"
+#include "CPlayerTPP.h"
+
+/* 419BA0-419BA1 00001	*/
+void CKrysztal::OnActivateLevel() {
 }
 
-/* 432DD0-432DEE 0001E	*/
-CKrysztal* CKrysztal::scalar_destructor(uchar param_1) {
-	return 0;
+/* 432DB0-432DD0 00020	*/
+CKrysztal::CKrysztal() {
+	unk_2658 = false;
+	unk_2659 = false;
 }
 
 /* 432DF0-432DFB 0000B	*/
@@ -14,17 +18,85 @@ CKrysztal::~CKrysztal() {
 }
 
 /* 432E00-432E5A 0005A	*/
-void* CKrysztal::EXP() {
-	return 0;
+void*  CKrysztal::EXP() {
+	return new CKrysztal();
+}
+
+/* 432E60-432E65 00005	*/
+void CKrysztal::OnActivate() {
+	OnActivateInternal();
+}
+
+/* 432E70-432F12 000A2	*/
+void CKrysztal::OnActivateInternal() {
+	EnableCollisionObjects(true);
+	SetCollisionType(2);
+	SetCollAction(0);
+	EnableRendering(true);
+	EnableCollision(true);
+	SetGravityAcceleration(0.0f);
+	SetFreeRotationMatrix(true);
+	SetAngSpeed(D3DXVECTOR3(0.0f, 100.0f, 0.0f));
+	EnableCallHandler("OnCollisionObject");
+	unk_2658 = false;
+	unk_2659 = true;
+}
+
+/* 432F20-432F65 00045	*/
+void CKrysztal::FUN00432f20() {
+	EnableCollisionObjects(false);
+	SetCollAction(0);
+	EnableRendering(false);
+	SetFreeRotationMatrix(false);
+	DisableCallHandler("OnCollisionObject");
+	unk_2658 = true;
+	unk_2659 = false;
 }
 
 /* 432F70-432FC3 00053	*/
-uchar CKrysztal::OnCollisionObject(int* param_1) {
-	return 0;
+void CKrysztal::OnCollisionObject(cMagMeshObject* param_1) {
+	if (strcmpi("CPlayerTPP", param_1->GetClassNameA()) != 0) {
+		return;
+	}
+
+	((GameSDK*) GetGame())->unk_276c = 1;
+	FUN00432f20();
+	((CPlayerTPP*) GetPlayerObject())->unk_3990->FUN0042d730(true);
+}
+
+/* 432FD0-43308D 000BD	*/
+void CKrysztal::Save(std::ostream& param_1) {
+	_ED_CHUNK chunk;
+	chunk.chunkType = GetObjectID();
+	chunk.chunkDataSize = sizeof(CKrysztalSaveData);
+	chunk.chunkEndPos = (int)param_1.tellp() + chunk.chunkDataSize + sizeof(_ED_CHUNK);
+	param_1.write((char*) &chunk, sizeof(_ED_CHUNK));
+
+	CKrysztalSaveData saveData;
+	saveData.unk_04 = unk_2659;
+	saveData.unk_00 = unk_2658;
+	saveData.unk_08 = ((GameSDK*) GetGame())->unk_276c;
+	param_1.write((char*) &saveData, sizeof(CKrysztalSaveData));
 }
 
 /* 433090-433102 00072	*/
-uchar CKrysztal::Load(void* param_1, int param_2, int param_3, uint param_4) {
-	return 0;
-}
+void CKrysztal::Load(std::ifstream& param_1, _ED_CHUNK param_2) {
+	if (param_2.chunkDataSize != sizeof(CKrysztalSaveData)) {
+		return;
+	}
+	CKrysztalSaveData saveData;
+	param_1.read((char*) &saveData, sizeof(CKrysztalSaveData));
+	unk_2658 = saveData.unk_00;
+	unk_2659 = saveData.unk_04;
 
+	((GameSDK*) GetGame())->unk_276c = saveData.unk_08;
+
+	if (unk_2659) {
+		OnActivateInternal();
+	}
+
+	if (unk_2658) {
+		FUN00432f20();
+	}
+
+}
