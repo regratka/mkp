e427d6b2c6ccfc197d83ba0df9aad713ed2a718a
Aszman <lukaszstarzyk1@gmail.com>
2026-06-28 13:38:22 +0200

    matched CKlucz

--- src/mkp/CKlucz.cpp
+++ src/mkp/CKlucz.cpp
@@ -1,12 +1,16 @@
 #include "CKlucz.h"
 
-/* 432B30-432B50 00020	*/
-CKlucz::CKlucz() {
+#include "GameSDK.h"
+#include "CPlayerTPP.h"
+
+/* 419BA0-419BA1 00001	*/
+void CKlucz::OnActivateLevel() {
 }
 
-/* 432B50-432B6E 0001E	*/
-CKlucz* CKlucz::scalar_destructor(uchar param_1) {
-	return 0;
+/* 432B30-432B50 00020	*/
+CKlucz::CKlucz() {
+	unk_2658 = false;
+	unk_2659 = false;
 }
 
 /* 432B70-432B7B 0000B	*/
@@ -15,26 +19,91 @@ CKlucz::~CKlucz() {
 
 /* 432B80-432BDA 0005A	*/
 void* CKlucz::EXP() {
-	return 0;
+	return new CKlucz();
 }
 
 /* 432BE0-432C33 00053	*/
-uchar CKlucz::OnCollisionObject(int* param_1) {
-	return 0;
+void CKlucz::OnCollisionObject(cMagMeshObject* param_1) {
+	if (strcmpi("CPlayerTPP", param_1->GetClassNameA()) != 0) {
+		return;
+	}
+
+	((GameSDK*) GetGame())->unk_276c = 1;
+	Hide();
+	((CPlayerTPP*) GetPlayerObject())->unk_3990->FUN0042d730(true);
 }
 
 /* 432C40-432CFB 000BB	*/
-uchar CKlucz::Load(void* param_1, int param_2, int param_3, uint param_4) {
-	return 0;
+void CKlucz::Load(std::ifstream& param_1, _ED_CHUNK param_2) {
+	if (param_2.chunkDataSize != sizeof(CKluczSaveData)) {
+		return;
+	}
+	CKluczSaveData saveData;
+	param_1.read((char*) &saveData, sizeof(CKluczSaveData));
+	unk_2658 = saveData.unk_00;
+	unk_2659 = saveData.unk_04;
+
+	((GameSDK*) GetGame())->unk_276c = saveData.unk_08;
+
+	if (((GameSDK*) GetGame())->unk_276c == 1) {
+		((CPlayerTPP*) GetPlayerObject())->unk_3990->FUN0042d750(true);
+	} else if (((GameSDK*) GetGame())->unk_276c == 0 
+			|| ((GameSDK*) GetGame())->unk_276c == -1) {
+		((CPlayerTPP*) GetPlayerObject())->unk_3990->FUN0042d750(false);
+	}
+
+	if (unk_2659) {
+		Show();
+	}
+
+	if (unk_2658) {
+		Hide();
+	}
+}
+
+/* 432E60-432E65 00005	*/
+void CKlucz::OnActivate() {
+	Show();
 }
 
 /* 432E70-432F12 000A2	*/
-uchar CKlucz::OnActivate() {
-	return 0;
+void CKlucz::Show() {
+	EnableCollisionObjects(true);
+	SetCollisionType(2);
+	SetCollAction(0);
+	EnableRendering(true);
+	EnableCollision(true);
+	SetGravityAcceleration(0.0f);
+	SetFreeRotationMatrix(true);
+	SetAngSpeed(D3DXVECTOR3(0.0f, 100.0f, 0.0f));
+	EnableCallHandler("OnCollisionObject");
+	unk_2658 = false;
+	unk_2659 = true;
+}
+
+/* 432F20-432F65 00045	*/
+void CKlucz::Hide() {
+	EnableCollisionObjects(false);
+	SetCollAction(0);
+	EnableRendering(false);
+	SetFreeRotationMatrix(false);
+	DisableCallHandler("OnCollisionObject");
+	unk_2658 = true;
+	unk_2659 = false;
 }
 
 /* 432FD0-43308D 000BD	*/
-uchar CKlucz::Save(int* param_1) {
-	return 0;
+void CKlucz::Save(std::ostream& param_1) {
+	_ED_CHUNK chunk;
+	chunk.chunkType = GetObjectID();
+	chunk.chunkDataSize = sizeof(CKluczSaveData);
+	chunk.chunkEndPos = (int)param_1.tellp() + chunk.chunkDataSize + sizeof(_ED_CHUNK);
+	param_1.write((char*) &chunk, sizeof(_ED_CHUNK));
+
+	CKluczSaveData saveData;
+	saveData.unk_04 = unk_2659;
+	saveData.unk_00 = unk_2658;
+	saveData.unk_08 = ((GameSDK*) GetGame())->unk_276c;
+	param_1.write((char*) &saveData, sizeof(CKluczSaveData));
 }
 
