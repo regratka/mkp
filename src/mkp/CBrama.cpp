1c3d734df5592eb47b544ea7f26229e0ad8bdec7
Aszman <lukaszstarzyk1@gmail.com>
2026-07-03 11:58:52 +0200

    improved OnProcessStateMachine match and brackets reposition

--- src/mkp/CBrama.cpp
+++ src/mkp/CBrama.cpp
@@ -1,18 +1,15 @@
 #include "CBrama.h"
 
 /* 4164E0-4164E3 00003	*/
-void CBrama::OnCollisionObject(cMagMeshObject *param_1)
-{
+void CBrama::OnCollisionObject(cMagMeshObject *param_1) {
 }
 
 /* 419BA0-419BA1 00001	*/
-void CBrama::OnFrame()
-{
+void CBrama::OnFrame() {
 }
 
 /* 41A860-41A8A3 00043	*/
-CBrama::CBrama()
-{
+CBrama::CBrama() {
 	unk_2748 = 0;
 	unk_274c = false;
 	unk_274d = false;
@@ -22,46 +19,37 @@ CBrama::CBrama()
 }
 
 /* 41A8D0-41A8DB 0000B	*/
-CBrama::~CBrama()
-{
+CBrama::~CBrama() {
 }
 
 /* 41A8E0-41A93A 0005A	*/
-void *CBrama::EXP()
-{
+void *CBrama::EXP() {
 	return new CBrama();
 }
 
 /* 41A940-41A96D 0002D	*/
-void CBrama::OnActivate()
-{
+void CBrama::OnActivate() {
 	unk_2734 = GetTickCount() * 0.001f;
 }
 
 /* 41A970-41AAC7 00157	*/
-void CBrama::OnActivateLevel()
-{
+void CBrama::OnActivateLevel() {
 	unk_2750 = GetPosition();
 	unk_275c = GetPosition();
-	if (meshData.fWysokosc <= 0.0f)
-	{
+	if (meshData.fWysokosc <= 0.0f) {
 		meshData.fWysokosc = 350.0f;
 	}
-	if (meshData.fPredkosc <= 0.0f)
-	{
+	if (meshData.fPredkosc <= 0.0f) {
 		meshData.fPredkosc = 500.0f;
 	}
 	EnableFrustumPhysicsPause(false);
-	if (meshData.SoundRangesMin == 0)
-	{
+	if (meshData.SoundRangesMin == 0) {
 		meshData.SoundRangesMin = 500;
 	}
-	if (meshData.SoundRangesMax == 0)
-	{
+	if (meshData.SoundRangesMax == 0) {
 		meshData.SoundRangesMax = 3000;
 	}
-	if (meshData.SoundName[0] != NULL)
-	{
+	if (meshData.SoundName[0] != NULL) {
 		unk_2768 = LoadSound(meshData.SoundName);
 		StopSound(unk_2768);
 		SetVolume(unk_2768, 1.0f);
@@ -70,16 +58,14 @@ void CBrama::OnActivateLevel()
 		unk_276c = true;
 	}
 
-	if (stateObject->unkn_08 != 1)
-	{
+	if (stateObject->unkn_08 != 1) {
 		stateObject->unkn_0c = 1;
 		stateObject->unkn_10 = true;
 	}
 }
 
 /* 41AAD0-41AAF1 00021	*/
-void CBrama::OnCrash()
-{
+void CBrama::OnCrash() {
 	if (stateObject->unkn_08 != 1) {
 		stateObject->unkn_0c = 1;
 		stateObject->unkn_10 = true;
@@ -87,8 +73,7 @@ void CBrama::OnCrash()
 }
 
 /* 41AB00-41AEEF 003EF	*/
-bool CBrama::OnProcessStateMachine(StateObject_Str *param_1, int param_2, MsgObject *param_3)
-{
+bool CBrama::OnProcessStateMachine(StateObject_Str *param_1, int param_2, MsgObject *param_3) {
 	bool res;
 	if (param_2 == 0) {
 		res = param_3->unkn_00 == 1;
@@ -108,8 +93,7 @@ bool CBrama::OnProcessStateMachine(StateObject_Str *param_1, int param_2, MsgObj
 			D3DXVECTOR3 diff = GetPosition() - unk_2750;
 			if (meshData.fWysokosc < D3DXVec3Length(&diff)) {
 				SetSpeedValue(0.0f);
-				if (param_1->unkn_08 != 2)
-				{
+				if (param_1->unkn_08 != 2) {
 					param_1->unkn_0c = 2;
 					param_1->unkn_10 = true;
 				}
@@ -118,34 +102,26 @@ bool CBrama::OnProcessStateMachine(StateObject_Str *param_1, int param_2, MsgObj
 		}
 		res = param_3->unkn_00 == 2;
 	} else if (param_2 == 2) {
-		if (param_3->unkn_00 == 1)
-		{
+		if (param_3->unkn_00 == 1) {
 			SetSpeedValue(100.0f);
 			unk_2750 = GetPosition();
-			if (unk_276c)
-			{
+			if (unk_276c) {
 				PlaySoundA(unk_2768, false);
 			}
 			return true;
 		}
-		if (param_3->unkn_00 == 3)
-		{
-			if (meshData.RuchWPoziomie != 0)
-			{
+		if (param_3->unkn_00 == 3) {
+			if (meshData.RuchWPoziomie != 0) {
 				SetSpeed(-GetRight() * meshData.fPredkosc);
-			}
-			else
-			{
+			} else {
 				SetSpeed(-GetUp() * meshData.fPredkosc);
 			}
 			D3DXVECTOR3 diff = GetPosition() - unk_2750;
-			if (meshData.fWysokosc < D3DXVec3Length(&diff))
-			{
+			if (meshData.fWysokosc < D3DXVec3Length(&diff)) {
 				SetPosition(unk_275c);
 				SetSpeedValue(0.0f);
 				SetPosition(unk_275c);
-				if (param_1->unkn_08 != 1)
-				{
+				if (param_1->unkn_08 != 1) {
 					param_1->unkn_0c = 1;
 					param_1->unkn_10 = true;
 				}
@@ -156,17 +132,14 @@ bool CBrama::OnProcessStateMachine(StateObject_Str *param_1, int param_2, MsgObj
 	} else if (param_2 == 4) {
 		res = param_3->unkn_00 == 1;
 	} else {
-		return false;
+		res = false;
 	}
-
 	return res;
 }
 
 /* 41AEF0-41AF1A 0002A	*/
-void CBrama::OnLoadChunk(_ED_CHUNK param_1, std::ifstream &param_2)
-{
-	if (param_1.chunkType == 15 && param_1.chunkDataSize == sizeof(CBramaMeshData))
-	{
+void CBrama::OnLoadChunk(_ED_CHUNK param_1, std::ifstream &param_2) {
+	if (param_1.chunkType == 15 && param_1.chunkDataSize == sizeof(CBramaMeshData)) {
 		param_2.read((char *)&meshData, sizeof(CBramaMeshData));
 	}
 }
