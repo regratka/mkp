c6e5fc9cdcce9e9766eb74ef3908f17653e9432b
Aszman <lukaszstarzyk1@gmail.com>
2026-07-12 20:07:49 +0200

    pgString initial progress

--- src/me2/pgString.cpp
+++ src/me2/pgString.cpp
@@ -1,99 +1,221 @@
 #include "pgString.h"
 
-/* 1000B420-1000B429 00009	*/
-pgString::pgString() {
-}
-
-/* 1000B430-1000B435 00005	*/
-int pgString::getLength() {
-	return 0;
-}
-
-/* 1000B450-1000B45C 0000C	*/
-char pgString::operator[](int param_1) {
-	return 0;
-}
+#include <stdio.h>
+#include <stdarg.h>
+#include <cctype>
 
 /* 100821E0-100821F9 00019	*/
-pgString::pgString(pgString* param_1) {
+pgString::pgString(pgString const& param_1) {
+	text = NULL;
+	*this = param_1;
 }
 
 /* 10082200-10082259 00059	*/
-pgString::pgString(char* param_1) {
+pgString::pgString(char const* param_1) {
+	if (param_1 != NULL) {
+		text = new char[strlen(param_1)+1];
+		strcpy(text, param_1);
+	} else {
+		text = NULL;
+	}
 }
 
 /* 10082260-1008226E 0000E	*/
 pgString::~pgString() {
+	if (text != NULL) {
+		delete[] text;
+	}
 }
 
 /* 10082270-10082304 00094	*/
-pgString* pgString::set(char* param_1, ...) {
-	return 0;
+pgString& pgString::set(char const* param_1, ...) {
+	if (text != NULL) {
+		delete[] text;
+	}
+
+	if (param_1 != NULL) {
+		va_list args;
+		va_start(args, param_1);
+		char buffor[2000];
+		vsprintf(buffor, param_1, args);
+
+		text = new char[strlen(buffor)+1];
+		strcpy(text, buffor);
+	} else {
+		text = NULL;
+	}
+
+	return *this;
 }
 
 /* 10082310-100823E2 000D2	*/
-pgString* pgString::cat(char* param_1) {
-	return 0;
+pgString& pgString::cat(char const* param_1) {
+	if (text == NULL) {
+		text = new char[strlen(param_1)+1];
+		strcpy(text, param_1);
+	} else {
+		char * oldText = text;
+		text = new char[strlen(text) + strlen(param_1) + 1];
+		strcpy(text, oldText);
+		strcat(text, param_1);
+		delete[] oldText;
+	}
+	return *this;
 }
 
 /* 100823F0-10082409 00019	*/
-int pgString::length() {
+int pgString::length() const {
+	if (text != NULL) {
+		return strlen(text);
+	}
 	return 0;
 }
 
 /* 10082410-10082481 00071	*/
-bool pgString::consistsJustOf(pgString* param_1) {
-	return 0;
+bool pgString::consistsJustOf(pgString const& param_1) const {
+	if (text == NULL) {
+		return true;
+	}
+	int index1 = 0;
+	while (index1 < length()) {
+		int index2 = 0;
+		bool bVar1 = false;
+		while (index2 < param_1.length()) {
+			if (text[index1] == param_1.text[index2]) {
+				bVar1 = true;
+			}
+			index2++;
+		}
+		if (!bVar1) {
+			return false;
+		}
+		index1++;
+	}
+	return true;
 }
 
 /* 10082490-100824C6 00036	*/
-int pgString::getIndex(char param_1, bool param_2) {
-	return 0;
+int pgString::getIndex(char param_1, bool param_2) const {
+	if (param_2) {
+		return getIndex(0, param_1, param_2);
+	} else {
+		return getIndex(length(), param_1, param_2);
+	}
 }
 
 /* 100824D0-1008254C 0007C	*/
-int pgString::getIndex(int param_1, char param_2, bool param_3) {
-	return 0;
+int pgString::getIndex(int param_1, char param_2, bool param_3) const {
+	if (param_1 == -1) {
+		if (param_3) {
+			param_1 = 0;
+		} else {
+			param_1 = length()-1;
+		}
+	}
+
+	if (text == NULL) {
+		return -1;
+	}
+
+	if (param_3) {
+		while(param_1 < length()) {
+			if (text[param_1] == param_2) {
+				return param_1;
+			}
+			param_1++;
+		}
+	} else {
+		while (param_1 >= 0) {
+			if (text[param_1] == param_2) {
+				return param_1;
+			}
+			param_1--;
+		}
+	}
+
+	return -1;
 }
 
 /* 10082550-10082586 00036	*/
-int pgString::getIndexNot(char param_1, bool param_2) {
-	return 0;
+int pgString::getIndexNot(char param_1, bool param_2) const {
+	if (param_2) {
+		return getIndexNot(0, param_1, param_2);
+	} else {
+		return getIndexNot(length(), param_1, param_2);
+	}
 }
 
 /* 10082590-1008260C 0007C	*/
-int pgString::getIndexNot(int param_1, char param_2, bool param_3) {
+int pgString::getIndexNot(int param_1, char param_2, bool param_3) const {
 	return 0;
 }
 
 /* 10082610-10082647 00037	*/
-int pgString::getIndex(pgString* param_1, bool param_2) {
-	return 0;
+int pgString::getIndex(pgString const& param_1, bool param_2) const {
+	if (param_2) {
+		return getIndex(0, param_1, param_2);
+	} else {
+		return getIndex(length()-1, param_1, param_2);
+	}
 }
 
 /* 10082650-10082732 000E2	*/
-int pgString::getIndex(int param_1, pgString* param_2, bool param_3) {
+int pgString::getIndex(int param_1, pgString const& param_2, bool param_3) const {
 	return 0;
 }
 
 /* 10082740-10082782 00042	*/
-int pgString::getCNum(char param_1) {
-	return 0;
+int pgString::getCNum(char param_1) const {
+	int iVar2 = 0;
+	if (text != NULL) {
+		for (int index = 0; index < length(); index++) {
+			if (text[index] == param_1) {
+				iVar2++;
+			}
+		}
+	}
+	return iVar2;
 }
 
 /* 10082790-10082850 000C0	*/
-pgString* pgString::getSubString(pgString param_1, int param_2, int param_3) {
-	return 0;
+pgString pgString::getSubString(int param_1, int param_2) const {
+	int len = param_2;
+	if (param_2 == -1) {
+		len = length() - param_1;
+	}
+
+	char* pcVar1 = new char[len+1];
+	for (int index = 0; index < len; index++) {
+		pcVar1[index] = text[index + param_1];
+	}
+	pcVar1[len+1] = '\0';
+	pgString res;
+	res.setBuffer(pcVar1);
+	return res;
 }
 
 /* 10082850-10082887 00037	*/
-pgString* pgString::toLower() {
-	return 0;
+pgString& pgString::toLower() {
+	char* c = text;
+	while (*c != '\0') {
+		if (isupper(*c)) {
+			*c = tolower(*c);
+		}
+		c++;
+	}
+	return *this;
 }
 
 /* 10082890-100828C7 00037	*/
-pgString* pgString::toUpper() {
-	return 0;
+pgString& pgString::toUpper() {
+	char* c = text;
+	while (*c != '\0') {
+		if (islower(*c)) {
+			*c = toupper(*c);
+		}
+		c++;
+	}
+	return *this;
 }
 
 /* 100828D0-100828FB 0002B	*/
@@ -162,8 +284,8 @@ bool pgString::operator!=(char* param_1) {
 }
 
 /* 10082E90-10082EF5 00065	*/
-pgString* pgString::operator=(pgString* param_1) {
-	return 0;
+pgString& pgString::operator=(pgString const& param_1) {
+	return *this;
 }
 
 /* 10082F00-10082F20 00020	*/
@@ -175,8 +297,4 @@ pgString* pgString::operator+=(pgString* param_1) {
 void pgString::setBuffer(char* param_1) {
 }
 
-/* 10110CD3-10110CFB 00028	*/
-int pgString::_islower(int param_1) {
-	return 0;
-}
 
