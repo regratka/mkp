358bf575f7bf8345ad52fd6eea8ad308b5bcf828
Aszman <lukaszstarzyk1@gmail.com>
2026-08-03 22:12:21 +0200

    defined variables and matched  OnActivate & FUN00435b60

--- include/mkp/MenuOknoWyboru_CZ1.h
+++ include/mkp/MenuOknoWyboru_CZ1.h
@@ -5,13 +5,15 @@
 
 #include "cMagSprite.h"
 #include "MenuModule.h"
+#include "UIWindow.h"
 
-class MenuOknoWyboru_CZ1 : public cMagSprite {
+class MenuOknoWyboru_CZ1 : public cMagSprite
+{
 public:
 	/* 435380 */ MenuOknoWyboru_CZ1();
 	/* 435480 */ virtual ~MenuOknoWyboru_CZ1();
 	/* 435490 */ void OnActivate();
-	/* 435B60 */ void FUN00435b60(MenuModule* param_1);
+	/* 435B60 */ void FUN00435b60(MenuModule *param_1);
 	/* 435B80 */ void OnFrame();
 	/* 435C90 */ void FUN00435c90();
 	/* 435D40 */ void FUN00435d40();
@@ -24,6 +26,49 @@ public:
 	/* 436420 */ void FUN00436420();
 	/* 436450 */ void FUN00436450();
 	/* 4368C0 */ void FUN004368c0();
+
+private:
+	/* 0x1740 */ void *unk_1740;
+	/* 0x1744 */ bool unk_1744;
+	/* 0x1748 */ MenuModule *menuModule;
+	/* 0x174c */ UIWindow *uiWindow;
+	/* 0x1750 */ int doorTexID;
+	/* 0x1754 */ int doorActiveTexID;
+	/* 0x1758 */ int unk_1758;
+	/* 0x175c */ int unk_175c;
+	/* 0x1760 */ int cursorsTextureIDs[5];
+	/* 0x1774 */ bool unk_1774;
+	/* 0x1778 */ int activeCursorIndex;
+	/* 0x177c */ float lastCursorChangeTime;
+	/* 0x1780 */ bool unk_1780;
+	/* 0x1781 */ uchar unk_1781[7];
+	/* 0x1788 */ float cursorXPos;
+	/* 0x178c */ float cursorYPos;
+	/* 0x1790 */ uchar unk_1790[12];
+	/* 0x179c */ int unk_179c;
+	/* 0x17a0 */ int lev1TexID;
+	/* 0x17a4 */ int lev2TexID;
+	/* 0x17a8 */ int lev3TexID;
+	/* 0x17ac */ int lev4TexID;
+	/* 0x17b0 */ int lev5TexID;
+	/* 0x17b4 */ int lev6TexID;
+	/* 0x17b8 */ int unk_17b8;
+	/* 0x17bc */ int deactLev1TexID;
+	/* 0x17c0 */ int deactLev2TexID;
+	/* 0x17c4 */ int deactLev3TexID;
+	/* 0x17c8 */ int deactLev4TexID;
+	/* 0x17cc */ int deactLev5TexID;
+	/* 0x17d0 */ int deactLev6TexID;
+	/* 0x17d4 */ bool unk_17d4;
+	/* 0x17d5 */ bool unk_17d5;
+	/* 0x17d6 */ bool unk_17d6;
+	/* 0x17d7 */ bool unk_17d7;
+	/* 0x17d8 */ bool unk_17d8;
+	/* 0x17d9 */ bool unk_17d9;
+	/* 0x17da */ bool unk_17da;
+	/* 0x17db */ uchar unk_17db[5];
 };
+
+STATIC_ASSERT(sizeof(MenuOknoWyboru_CZ1) == 0x17e0);
 
 #endif
--- src/mkp/MenuOknoWyboru_CZ1.cpp
+++ src/mkp/MenuOknoWyboru_CZ1.cpp
@@ -1,7 +1,38 @@
 #include "MenuOknoWyboru_CZ1.h"
+#include "GameSDK.h"
 
 /* 435380-435451 000D1	*/
 MenuOknoWyboru_CZ1::MenuOknoWyboru_CZ1() {
+    unk_179c = -1;
+    lev1TexID = -1;
+    lev2TexID = -1;
+    lev3TexID = -1;
+    lev4TexID = -1;
+    lev5TexID = -1;
+    lev6TexID = -1;
+    unk_1758 = -1;
+    unk_17b8 = -1;
+    deactLev1TexID = -1;
+    deactLev2TexID = -1;
+    deactLev3TexID = -1;
+    deactLev4TexID = -1;
+    deactLev5TexID = -1;
+    deactLev6TexID = -1;
+    unk_17d4 = true;
+    unk_17d5 = true;
+    unk_1744 = true;
+    unk_175c = 1;
+    uiWindow = (UIWindow *)0x0;
+    menuModule = (MenuModule *)0x0;
+    unk_17d6 = false;
+    unk_17d7 = false;
+    unk_17d8 = false;
+    unk_17d9 = false;
+    unk_17da = false;
+    activeCursorIndex = 0;
+    lastCursorChangeTime = 0.0f;
+    unk_1780 = false;
+    unk_1774 = false;
 }
 
 /* 435480-43548C 0000C	*/
@@ -10,10 +41,107 @@ MenuOknoWyboru_CZ1::~MenuOknoWyboru_CZ1() {
 
 /* 435490-435B51 006C1	*/
 void MenuOknoWyboru_CZ1::OnActivate() {
+    unk_1740 = ((GameSDK *)GetGame())->unk_2548;
+
+    uiWindow = new UIWindow(this);
+    CreateObject(uiWindow);
+    uiWindow->FUN004134e0("data\\textures\\menu\\ramka.png");
+    uiWindow->FUN00413540(D3DXVECTOR2(1.0f, 0.8f));
+    uiWindow->FUN00413560(D3DXVECTOR2(0.552f, 0.466f));
+    uiWindow->FUN00413580(false);
+
+    doorTexID = uiWindow->AddTexture("data\\textures\\hud\\drzwi.png");
+    uiWindow->SetScale(doorTexID, 0.7f, 0.7f);
+    uiWindow->SetPos(doorTexID, 0.823f, 0.163f, 0);
+    uiWindow->Hide(doorTexID);
+    uiWindow->EnableCheckMouse(doorTexID, true);
+
+    doorActiveTexID = uiWindow->AddTexture("data\\textures\\hud\\drzwi_a.png");
+    uiWindow->SetScale(doorActiveTexID, 0.7f, 0.7f);
+    uiWindow->SetPos(doorActiveTexID, 0.823f, 0.163f, 0);
+    uiWindow->Hide(doorActiveTexID);
+    uiWindow->EnableCheckMouse(doorActiveTexID, true);
+
+    lev1TexID = uiWindow->AddTexture("data\\textures\\menu\\l1.png");
+    uiWindow->SetScale(lev1TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(lev1TexID, 0.4f, 0.34f, 0);
+    uiWindow->Hide(lev1TexID);
+    uiWindow->EnableCheckMouse(lev1TexID, true);
+
+    deactLev1TexID = uiWindow->AddTexture("data\\textures\\menu\\l1_deact.png");
+    uiWindow->SetScale(deactLev1TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(deactLev1TexID, 0.4f, 0.34f, 0);
+    uiWindow->Hide(deactLev1TexID);
+    uiWindow->EnableCheckMouse(deactLev1TexID, true);
+
+    lev2TexID = uiWindow->AddTexture("data\\textures\\menu\\l2.png");
+    uiWindow->SetScale(lev2TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(lev2TexID, 0.56f, 0.34f, 0);
+    uiWindow->Hide(lev2TexID);
+    uiWindow->EnableCheckMouse(lev2TexID, true);
+
+    deactLev2TexID = uiWindow->AddTexture("data\\textures\\menu\\l2_deact.png");
+    uiWindow->SetScale(deactLev2TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(deactLev2TexID, 0.56f, 0.34f, 0);
+    uiWindow->Hide(deactLev2TexID);
+    uiWindow->EnableCheckMouse(deactLev2TexID, true);
+
+    lev3TexID = uiWindow->AddTexture("data\\textures\\menu\\l3.png");
+    uiWindow->SetScale(lev3TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(lev3TexID, 0.72f, 0.34f, 0);
+    uiWindow->Hide(lev3TexID);
+    uiWindow->EnableCheckMouse(lev3TexID, true);
+
+    deactLev3TexID = uiWindow->AddTexture("data\\textures\\menu\\l3_deact.png");
+    uiWindow->SetScale(deactLev3TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(deactLev3TexID, 0.72f, 0.34f, 0);
+    uiWindow->Hide(deactLev3TexID);
+    uiWindow->EnableCheckMouse(deactLev3TexID, true);
+
+    lev4TexID = uiWindow->AddTexture("data\\textures\\menu\\l4.png");
+    uiWindow->SetScale(lev4TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(lev4TexID, 0.4f, 0.55f, 0);
+    uiWindow->Hide(lev4TexID);
+    uiWindow->EnableCheckMouse(lev4TexID, true);
+
+    deactLev4TexID = uiWindow->AddTexture("data\\textures\\menu\\l4_deact.png");
+    uiWindow->SetScale(deactLev4TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(deactLev4TexID, 0.4f, 0.55f, 0);
+    uiWindow->Hide(deactLev4TexID);
+    uiWindow->EnableCheckMouse(deactLev4TexID, true);
+
+    lev5TexID = uiWindow->AddTexture("data\\textures\\menu\\l5.png");
+    uiWindow->SetScale(lev5TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(lev5TexID, 0.56f, 0.55f, 0);
+    uiWindow->Hide(lev5TexID);
+    uiWindow->EnableCheckMouse(lev5TexID, true);
+
+    deactLev5TexID = uiWindow->AddTexture("data\\textures\\menu\\l5_deact.png");
+    uiWindow->SetScale(deactLev5TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(deactLev5TexID, 0.56f, 0.55f, 0);
+    uiWindow->Hide(deactLev5TexID);
+    uiWindow->EnableCheckMouse(deactLev5TexID, true);
+
+    lev6TexID = uiWindow->AddTexture("data\\textures\\menu\\l6.png");
+    uiWindow->SetScale(lev6TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(lev6TexID, 0.72f, 0.55f, 0);
+    uiWindow->Hide(lev6TexID);
+    uiWindow->EnableCheckMouse(lev6TexID, true);
+
+    deactLev6TexID = uiWindow->AddTexture("data\\textures\\menu\\l6_deact.png");
+    uiWindow->SetScale(deactLev6TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(deactLev6TexID, 0.72f, 0.55f, 0);
+    uiWindow->Hide(deactLev6TexID);
+    uiWindow->EnableCheckMouse(deactLev6TexID, true);
+
+    FUN004368c0();
 }
 
+
+
 /* 435B60-435B6D 0000D	*/
-void MenuOknoWyboru_CZ1::FUN00435b60(MenuModule* param_1) {
+void MenuOknoWyboru_CZ1::FUN00435b60(MenuModule *param_1) {
+    menuModule = param_1;
 }
 
 /* 435B80-435C81 00101	*/
@@ -29,38 +157,46 @@ void MenuOknoWyboru_CZ1::FUN00435d40() {
 }
 
 /* 435E60-435F9E 0013E	*/
-void MenuOknoWyboru_CZ1::OnMouseArrive(int param_1) {
+void MenuOknoWyboru_CZ1::OnMouseArrive(int param_1)
+{
 }
 
 /* 435FA0-435FE3 00043	*/
-void MenuOknoWyboru_CZ1::OnMouseLeave(int param_1) {
+void MenuOknoWyboru_CZ1::OnMouseLeave(int param_1)
+{
 }
 
 /* 435FF0-436158 00168	*/
-void MenuOknoWyboru_CZ1::OnInputMouse(float param_1, float param_2, bool param_3, bool param_4) {
+void MenuOknoWyboru_CZ1::OnInputMouse(float param_1, float param_2, bool param_3, bool param_4)
+{
 }
 
 /* 436160-436321 001C1	*/
-void MenuOknoWyboru_CZ1::OnActivateLevel() {
+void MenuOknoWyboru_CZ1::OnActivateLevel()
+{
 }
 
 /* 436330-4363D1 000A1	*/
-void MenuOknoWyboru_CZ1::FUN00436330() {
+void MenuOknoWyboru_CZ1::FUN00436330()
+{
 }
 
 /* 4363E0-43641B 0003B	*/
-void MenuOknoWyboru_CZ1::FUN004363e0() {
+void MenuOknoWyboru_CZ1::FUN004363e0()
+{
 }
 
 /* 436420-436447 00027	*/
-void MenuOknoWyboru_CZ1::FUN00436420() {
+void MenuOknoWyboru_CZ1::FUN00436420()
+{
 }
 
 /* 436450-4368BC 0046C	*/
-void MenuOknoWyboru_CZ1::FUN00436450() {
+void MenuOknoWyboru_CZ1::FUN00436450()
+{
 }
 
 /* 4368C0-436C14 00354	*/
-void MenuOknoWyboru_CZ1::FUN004368c0() {
+void MenuOknoWyboru_CZ1::FUN004368c0()
+{
 }
-
