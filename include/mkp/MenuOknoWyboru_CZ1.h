4fcc913079a3140874415c8b6f9b7e3d23273949
Łukasz Starzyk <72223520+Aszman@users.noreply.github.com>
2026-08-05 19:48:47 +0200

    matching MenuOknoWyboruCz1 (#21)
    * MenuOknoWyboru_CZ1 initial commit
    
    * defined variables and matched  OnActivate & FUN00435b60
    
    * matched everything except FUN004368c0
    
    * matched FUN004368c0
    
    * renamed methods and attributes

--- include/mkp/MenuOknoWyboru_CZ1.h
+++ include/mkp/MenuOknoWyboru_CZ1.h
@@ -2,19 +2,73 @@
 #define _MENU_OKNO_WYBORU__C_Z1
 
 #include <globals.h>
-#include "cMagSprite.h"
 
-class MenuOknoWyboru_CZ1 {
+#include "cMagSprite.h"
+#include "MenuModule.h"
+#include "UIWindow.h"
+
+class MenuOknoWyboru_CZ1 : public cMagSprite
+{
 public:
 	/* 435380 */ MenuOknoWyboru_CZ1();
-	/* 435460 */ cMagSprite* scalar_destructor(uchar param_1);
-	/* 435480 */ ~MenuOknoWyboru_CZ1();
+	/* 435480 */ virtual ~MenuOknoWyboru_CZ1();
 	/* 435490 */ void OnActivate();
+	/* 435B60 */ void SetMenuModule(MenuModule *param_1);
 	/* 435B80 */ void OnFrame();
-	/* 435E60 */ uchar OnMouseArrive(int param_1);
-	/* 435FA0 */ uchar OnMouseLeave(int param_1);
-	/* 435FF0 */ uchar OnInputMouse(uint param_1, uint param_2, char param_3);
+	/* 435C90 */ void OpenWindow();
+	/* 435D40 */ void CloseWindow();
+	/* 435E60 */ void OnMouseArrive(int param_1);
+	/* 435FA0 */ void OnMouseLeave(int param_1);
+	/* 435FF0 */ void OnInputMouse(float param_1, float param_2, bool param_3, bool param_4);
 	/* 436160 */ void OnActivateLevel();
+	/* 436330 */ void ChangeActiveCursor();
+	/* 4363E0 */ void ActivateDefaultCursor();
+	/* 436420 */ void ActivateStopCursor();
+	/* 436450 */ void ShowUnlockedLevels();
+	/* 4368C0 */ void LoadUnlockedLevels();
+
+private:
+	/* 0x1740 */ void *unk_1740;
+	/* 0x1744 */ bool unk_1744;
+	/* 0x1748 */ MenuModule *menuModule;
+	/* 0x174c */ UIWindow *uiWindow;
+	/* 0x1750 */ int doorTexID;
+	/* 0x1754 */ int doorActiveTexID;
+	/* 0x1758 */ int unk_1758;
+	/* 0x175c */ int unlockedLevels;
+	/* 0x1760 */ int cursorsTextureIDs[5];
+	/* 0x1774 */ bool isHoveringLockedLevel;
+	/* 0x1778 */ int activeCursorIndex;
+	/* 0x177c */ float lastCursorChangeTime;
+	/* 0x1780 */ bool isHoveringUnlockedLevel;
+	/* 0x1781 */ uchar unk_1781[7];
+	/* 0x1788 */ float cursorXPos;
+	/* 0x178c */ float cursorYPos;
+	/* 0x1790 */ uchar unk_1790[12];
+	/* 0x179c */ int unk_179c;
+	/* 0x17a0 */ int lev1TexID;
+	/* 0x17a4 */ int lev2TexID;
+	/* 0x17a8 */ int lev3TexID;
+	/* 0x17ac */ int lev4TexID;
+	/* 0x17b0 */ int lev5TexID;
+	/* 0x17b4 */ int lev6TexID;
+	/* 0x17b8 */ int unk_17b8;
+	/* 0x17bc */ int deactLev1TexID;
+	/* 0x17c0 */ int deactLev2TexID;
+	/* 0x17c4 */ int deactLev3TexID;
+	/* 0x17c8 */ int deactLev4TexID;
+	/* 0x17cc */ int deactLev5TexID;
+	/* 0x17d0 */ int deactLev6TexID;
+	/* 0x17d4 */ bool isIntroUnlocked;
+	/* 0x17d5 */ bool isLevel1Unlocked;
+	/* 0x17d6 */ bool isLevel2Unlocked;
+	/* 0x17d7 */ bool isLevel3Unlocked;
+	/* 0x17d8 */ bool isLevel4Unlocked;
+	/* 0x17d9 */ bool isLevel5Unlocked;
+	/* 0x17da */ bool isLevel6Unlocked;
+	/* 0x17db */ uchar unk_17db[5];
 };
+
+STATIC_ASSERT(sizeof(MenuOknoWyboru_CZ1) == 0x17e0);
 
 #endif
--- src/mkp/MenuOknoWyboru_CZ1.cpp
+++ src/mkp/MenuOknoWyboru_CZ1.cpp
@@ -1,12 +1,39 @@
 #include "MenuOknoWyboru_CZ1.h"
 
+#include "GameSDK.h"
+
 /* 435380-435451 000D1	*/
 MenuOknoWyboru_CZ1::MenuOknoWyboru_CZ1() {
-}
-
-/* 435460-43547E 0001E	*/
-cMagSprite* MenuOknoWyboru_CZ1::scalar_destructor(uchar param_1) {
-	return 0;
+    uiWindow = NULL;
+    menuModule = NULL;
+    unk_179c = -1;
+    lev1TexID = -1;
+    lev2TexID = -1;
+    lev3TexID = -1;
+    lev4TexID = -1;
+    lev5TexID = -1;
+    lev6TexID = -1;
+    unk_1758 = -1;
+    unk_17b8 = -1;
+    deactLev1TexID = -1;
+    deactLev2TexID = -1;
+    deactLev3TexID = -1;
+    deactLev4TexID = -1;
+    deactLev5TexID = -1;
+    deactLev6TexID = -1;
+    isIntroUnlocked = true;
+    isLevel1Unlocked = true;
+    isLevel2Unlocked = false;
+    isLevel3Unlocked = false;
+    isLevel4Unlocked = false;
+    isLevel5Unlocked = false;
+    isLevel6Unlocked = false;
+    unk_1744 = true;
+    unlockedLevels = 1;
+    activeCursorIndex = 0;
+    lastCursorChangeTime = 0.0f;
+    isHoveringUnlockedLevel = false;
+    isHoveringLockedLevel = false;
 }
 
 /* 435480-43548C 0000C	*/
@@ -15,28 +42,489 @@ MenuOknoWyboru_CZ1::~MenuOknoWyboru_CZ1() {
 
 /* 435490-435B51 006C1	*/
 void MenuOknoWyboru_CZ1::OnActivate() {
+    unk_1740 = ((GameSDK *)GetGame())->unk_2548;
+
+    uiWindow = new UIWindow(this);
+    CreateObject(uiWindow);
+    uiWindow->FUN004134e0("data\\textures\\menu\\ramka.png");
+    uiWindow->FUN00413540(D3DXVECTOR2(1.0f, 0.8f));
+    uiWindow->FUN00413560(D3DXVECTOR2(0.552f, 0.466f));
+    uiWindow->FUN00413580(false);
+
+    doorTexID = uiWindow->AddTexture("data\\textures\\hud\\drzwi.png");
+    uiWindow->SetScale(doorTexID, 0.7f, 0.7f);
+    uiWindow->SetPos(doorTexID, 0.823f, 0.163f, 0);
+    uiWindow->Hide(doorTexID);
+    uiWindow->EnableCheckMouse(doorTexID, true);
+
+    doorActiveTexID = uiWindow->AddTexture("data\\textures\\hud\\drzwi_a.png");
+    uiWindow->SetScale(doorActiveTexID, 0.7f, 0.7f);
+    uiWindow->SetPos(doorActiveTexID, 0.823f, 0.163f, 0);
+    uiWindow->Hide(doorActiveTexID);
+    uiWindow->EnableCheckMouse(doorActiveTexID, true);
+
+    lev1TexID = uiWindow->AddTexture("data\\textures\\menu\\l1.png");
+    uiWindow->SetScale(lev1TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(lev1TexID, 0.4f, 0.34f, 0);
+    uiWindow->Hide(lev1TexID);
+    uiWindow->EnableCheckMouse(lev1TexID, true);
+
+    deactLev1TexID = uiWindow->AddTexture("data\\textures\\menu\\l1_deact.png");
+    uiWindow->SetScale(deactLev1TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(deactLev1TexID, 0.4f, 0.34f, 0);
+    uiWindow->Hide(deactLev1TexID);
+    uiWindow->EnableCheckMouse(deactLev1TexID, true);
+
+    lev2TexID = uiWindow->AddTexture("data\\textures\\menu\\l2.png");
+    uiWindow->SetScale(lev2TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(lev2TexID, 0.56f, 0.34f, 0);
+    uiWindow->Hide(lev2TexID);
+    uiWindow->EnableCheckMouse(lev2TexID, true);
+
+    deactLev2TexID = uiWindow->AddTexture("data\\textures\\menu\\l2_deact.png");
+    uiWindow->SetScale(deactLev2TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(deactLev2TexID, 0.56f, 0.34f, 0);
+    uiWindow->Hide(deactLev2TexID);
+    uiWindow->EnableCheckMouse(deactLev2TexID, true);
+
+    lev3TexID = uiWindow->AddTexture("data\\textures\\menu\\l3.png");
+    uiWindow->SetScale(lev3TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(lev3TexID, 0.72f, 0.34f, 0);
+    uiWindow->Hide(lev3TexID);
+    uiWindow->EnableCheckMouse(lev3TexID, true);
+
+    deactLev3TexID = uiWindow->AddTexture("data\\textures\\menu\\l3_deact.png");
+    uiWindow->SetScale(deactLev3TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(deactLev3TexID, 0.72f, 0.34f, 0);
+    uiWindow->Hide(deactLev3TexID);
+    uiWindow->EnableCheckMouse(deactLev3TexID, true);
+
+    lev4TexID = uiWindow->AddTexture("data\\textures\\menu\\l4.png");
+    uiWindow->SetScale(lev4TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(lev4TexID, 0.4f, 0.55f, 0);
+    uiWindow->Hide(lev4TexID);
+    uiWindow->EnableCheckMouse(lev4TexID, true);
+
+    deactLev4TexID = uiWindow->AddTexture("data\\textures\\menu\\l4_deact.png");
+    uiWindow->SetScale(deactLev4TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(deactLev4TexID, 0.4f, 0.55f, 0);
+    uiWindow->Hide(deactLev4TexID);
+    uiWindow->EnableCheckMouse(deactLev4TexID, true);
+
+    lev5TexID = uiWindow->AddTexture("data\\textures\\menu\\l5.png");
+    uiWindow->SetScale(lev5TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(lev5TexID, 0.56f, 0.55f, 0);
+    uiWindow->Hide(lev5TexID);
+    uiWindow->EnableCheckMouse(lev5TexID, true);
+
+    deactLev5TexID = uiWindow->AddTexture("data\\textures\\menu\\l5_deact.png");
+    uiWindow->SetScale(deactLev5TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(deactLev5TexID, 0.56f, 0.55f, 0);
+    uiWindow->Hide(deactLev5TexID);
+    uiWindow->EnableCheckMouse(deactLev5TexID, true);
+
+    lev6TexID = uiWindow->AddTexture("data\\textures\\menu\\l6.png");
+    uiWindow->SetScale(lev6TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(lev6TexID, 0.72f, 0.55f, 0);
+    uiWindow->Hide(lev6TexID);
+    uiWindow->EnableCheckMouse(lev6TexID, true);
+
+    deactLev6TexID = uiWindow->AddTexture("data\\textures\\menu\\l6_deact.png");
+    uiWindow->SetScale(deactLev6TexID, 0.9f, 0.9f);
+    uiWindow->SetPos(deactLev6TexID, 0.72f, 0.55f, 0);
+    uiWindow->Hide(deactLev6TexID);
+    uiWindow->EnableCheckMouse(deactLev6TexID, true);
+
+    LoadUnlockedLevels();
+}
+
+
+
+/* 435B60-435B6D 0000D	*/
+void MenuOknoWyboru_CZ1::SetMenuModule(MenuModule *param_1) {
+    menuModule = param_1;
 }
 
 /* 435B80-435C81 00101	*/
 void MenuOknoWyboru_CZ1::OnFrame() {
+    if (isHoveringUnlockedLevel) {
+        ChangeActiveCursor();
+    } else if (isHoveringLockedLevel) {
+        ActivateStopCursor();
+    } else {
+        ActivateDefaultCursor();
+    }
+
+    POINT local_8;
+    GetCursorPos(&local_8);
+    ScreenToClient(GetHandleWindow(), &local_8);
+    int iVar1 = GetWindowWidth();
+    int iVar2 = GetWindowHeight();
+    cursorXPos = local_8.x / (float)iVar1;
+    cursorYPos = local_8.y / (float)iVar2;
+    uiWindow->SetScale(cursorsTextureIDs[activeCursorIndex], 1.0f, 1.0f);
+    uiWindow->SetPos(cursorsTextureIDs[activeCursorIndex], 
+        cursorXPos +  0.01f, cursorYPos + 0.01f, 0);
+    uiWindow->Show(cursorsTextureIDs[activeCursorIndex]);
+}
+
+/* 435C90-435D38 000A8	*/
+void MenuOknoWyboru_CZ1::OpenWindow() {
+    uiWindow->FUN00413580(true);
+    uiWindow->Show(doorTexID);
+    uiWindow->Show(lev1TexID);
+    uiWindow->Show(lev2TexID);
+    uiWindow->Show(lev3TexID);
+    uiWindow->Show(lev4TexID);
+    uiWindow->Show(lev5TexID);
+    uiWindow->Show(lev6TexID);
+    ShowUnlockedLevels();
+    EnableCallHandler("OnInputMouse");
+    EnableCallHandler("OnFrame");
+}
+
+/* 435D40-435E60 00120	*/
+void MenuOknoWyboru_CZ1::CloseWindow() {
+    ActivateDefaultCursor();
+    uiWindow->FUN00413580(false);
+    uiWindow->Hide(doorTexID);
+    uiWindow->Hide(doorActiveTexID);
+    uiWindow->Hide(lev1TexID);
+    uiWindow->Hide(lev2TexID);
+    uiWindow->Hide(lev3TexID);
+    uiWindow->Hide(lev4TexID);
+    uiWindow->Hide(lev5TexID);
+    uiWindow->Hide(lev6TexID);
+    uiWindow->Hide(deactLev1TexID);
+    uiWindow->Hide(deactLev2TexID);
+    uiWindow->Hide(deactLev3TexID);
+    uiWindow->Hide(deactLev4TexID);
+    uiWindow->Hide(deactLev5TexID);
+    uiWindow->Hide(deactLev6TexID);
+    DisableCallHandler("OnInputMouse");
+    DisableCallHandler("OnFrame");
+    if (menuModule != NULL) {
+        menuModule->FUN004349a0(false);
+    }
 }
 
 /* 435E60-435F9E 0013E	*/
-uchar MenuOknoWyboru_CZ1::OnMouseArrive(int param_1) {
-	return 0;
+void MenuOknoWyboru_CZ1::OnMouseArrive(int param_1) {
+    isHoveringUnlockedLevel = true;
+    if (unk_17b8 == param_1 || deactLev1TexID == param_1 || deactLev2TexID == param_1 || deactLev3TexID == param_1
+            || deactLev4TexID == param_1 || deactLev5TexID == param_1 || deactLev6TexID == param_1 ) {
+        isHoveringUnlockedLevel = false;
+        isHoveringLockedLevel = true;
+    }
+
+    if (lev1TexID == param_1 && !isLevel1Unlocked) {
+        isHoveringUnlockedLevel = false;
+        isHoveringLockedLevel = true;
+    }
+    if (lev2TexID == param_1 && !isLevel2Unlocked) {
+        isHoveringUnlockedLevel = false;
+        isHoveringLockedLevel = true;
+    }
+    if (lev3TexID == param_1 && !isLevel3Unlocked) {
+        isHoveringUnlockedLevel = false;
+        isHoveringLockedLevel = true;
+    }
+    if (lev4TexID == param_1 && !isLevel4Unlocked) {
+        isHoveringUnlockedLevel = false;
+        isHoveringLockedLevel = true;
+    }
+    if (lev5TexID == param_1 && !isLevel5Unlocked) {
+        isHoveringUnlockedLevel = false;
+        isHoveringLockedLevel = true;
+    }
+    if (lev6TexID == param_1 && !isLevel6Unlocked) {
+        isHoveringUnlockedLevel = false;
+        isHoveringLockedLevel = true;
+    }
+
+    if (doorTexID == param_1) {
+        uiWindow->Hide(doorTexID);
+        uiWindow->Show(doorActiveTexID);
+        if (menuModule != NULL) {
+            menuModule->FUN004351c0();
+        }
+    }
 }
 
 /* 435FA0-435FE3 00043	*/
-uchar MenuOknoWyboru_CZ1::OnMouseLeave(int param_1) {
-	return 0;
+void MenuOknoWyboru_CZ1::OnMouseLeave(int param_1) {
+    isHoveringUnlockedLevel = false;
+    isHoveringLockedLevel = false;
+    if (doorActiveTexID == param_1) {
+        uiWindow->Hide(doorActiveTexID);
+        uiWindow->Show(doorTexID);
+    }
 }
 
 /* 435FF0-436158 00168	*/
-uchar MenuOknoWyboru_CZ1::OnInputMouse(uint param_1, uint param_2, char param_3) {
-	return 0;
+void MenuOknoWyboru_CZ1::OnInputMouse(float param_1, float param_2, bool param_3, bool param_4) {
+    if (param_3 && !unk_1744) {
+        if (doorActiveTexID == uiWindow->GetMouseActivateSprite()) {
+            CloseWindow();
+        }
+        if (lev1TexID == uiWindow->GetMouseActivateSprite()) {
+            if (isLevel1Unlocked) {
+                GetGame()->LoadLevel("data\\levels\\Intro\\intro04.lev");
+            }
+        }
+        if (lev2TexID == uiWindow->GetMouseActivateSprite()) {
+            if (isLevel2Unlocked) {
+                GetGame()->LoadLevel("data\\levels\\Level02\\Level02a.lev");
+            }
+        }
+        if (lev3TexID == uiWindow->GetMouseActivateSprite() && isLevel3Unlocked) {
+            if (isLevel3Unlocked) {
+                GetGame()->LoadLevel("data\\levels\\Level03\\Level03.lev");
+            }
+        }
+        if (lev4TexID == uiWindow->GetMouseActivateSprite()) {
+            if (isLevel4Unlocked) {
+                GetGame()->LoadLevel("data\\levels\\Level04\\level04.lev");
+            }
+        }
+        if (lev5TexID == uiWindow->GetMouseActivateSprite()) {
+            if (isLevel5Unlocked) {
+                GetGame()->LoadLevel("data\\levels\\Level05\\Level05kk.lev");
+            }
+        }
+        if (lev6TexID == uiWindow->GetMouseActivateSprite()) {
+            if (isLevel6Unlocked) {
+                GetGame()->LoadLevel("data\\levels\\Level06\\Level06.lev");
+            }
+        } 
+    }
+    unk_1744 = param_3;
 }
 
 /* 436160-436321 001C1	*/
 void MenuOknoWyboru_CZ1::OnActivateLevel() {
+    cursorsTextureIDs[0] = uiWindow->AddTexture("data\\textures\\menu\\kursor.png");
+    uiWindow->SetScale(cursorsTextureIDs[0], 1.0f, 1.0f);
+    uiWindow->SetPos(cursorsTextureIDs[0], 0.1f, 0.1f, 0);
+    uiWindow->Hide(cursorsTextureIDs[0]);
+    
+    cursorsTextureIDs[1] = uiWindow->AddTexture("data\\textures\\menu\\kursor1.png");
+    uiWindow->SetScale(cursorsTextureIDs[1], 1.0f, 1.0f);
+    uiWindow->SetPos(cursorsTextureIDs[1], 0.1f, 0.1f, 0);
+    uiWindow->Hide(cursorsTextureIDs[1]);
+
+    cursorsTextureIDs[2] = uiWindow->AddTexture("data\\textures\\menu\\kursor2.png");
+    uiWindow->SetScale(cursorsTextureIDs[2], 1.0f, 1.0f);
+    uiWindow->SetPos(cursorsTextureIDs[2], 0.1f, 0.1f, 0);
+    uiWindow->Hide(cursorsTextureIDs[2]);
+
+    cursorsTextureIDs[3] = uiWindow->AddTexture("data\\textures\\menu\\kursor3.png");
+    uiWindow->SetScale(cursorsTextureIDs[3], 1.0f, 1.0f);
+    uiWindow->SetPos(cursorsTextureIDs[3], 0.1f, 0.1f, 0);
+    uiWindow->Hide(cursorsTextureIDs[3]);
+
+    cursorsTextureIDs[4] = uiWindow->AddTexture("data\\textures\\menu\\kursor_stop.png");
+    uiWindow->SetScale(cursorsTextureIDs[4], 1.0f, 1.0f);
+    uiWindow->SetPos(cursorsTextureIDs[4], 0.1f, 0.1f, 0);
+    uiWindow->Hide(cursorsTextureIDs[4]);
 }
 
+/* 436330-4363D1 000A1	*/
+void MenuOknoWyboru_CZ1::ChangeActiveCursor() {
+    for (int index = 1; index < 4; index++) {
+        uiWindow->Hide(cursorsTextureIDs[index]);
+    }
+    uiWindow->Hide(cursorsTextureIDs[0]);
+    float fVar1 = timeGetTime() * 0.001f;
+    if (fVar1 - lastCursorChangeTime > 0.1f) {
+        lastCursorChangeTime = fVar1;
+        activeCursorIndex++;
+        if (activeCursorIndex > 3) {
+            activeCursorIndex = 1;
+        }
+    }
+}
+
+/* 4363E0-43641B 0003B	*/
+void MenuOknoWyboru_CZ1::ActivateDefaultCursor() {
+    for (int i = 0; i < sizeof(cursorsTextureIDs)/sizeof(int); i++) {
+        uiWindow->Hide(cursorsTextureIDs[i]);
+    }
+    activeCursorIndex = 0;
+    isHoveringUnlockedLevel = false;
+}
+
+/* 436420-436447 00027	*/
+void MenuOknoWyboru_CZ1::ActivateStopCursor() {
+    ActivateDefaultCursor();
+    activeCursorIndex = 4;
+    uiWindow->Show(cursorsTextureIDs[activeCursorIndex]);
+}
+
+/* 436450-4368BC 0046C	*/
+void MenuOknoWyboru_CZ1::ShowUnlockedLevels() {
+    uiWindow->Hide(lev1TexID);
+    uiWindow->Hide(lev2TexID);
+    uiWindow->Hide(lev3TexID);
+    uiWindow->Hide(lev4TexID);
+    uiWindow->Hide(lev5TexID);
+    uiWindow->Hide(lev6TexID);
+    uiWindow->Hide(deactLev1TexID);
+    uiWindow->Hide(deactLev2TexID);
+    uiWindow->Hide(deactLev3TexID);
+    uiWindow->Hide(deactLev4TexID);
+    uiWindow->Hide(deactLev5TexID);
+    uiWindow->Hide(deactLev6TexID);
+
+    if (unlockedLevels == 1 || unlockedLevels == 0) {
+        uiWindow->Show(lev1TexID);
+        uiWindow->Hide(deactLev1TexID);
+        uiWindow->Show(deactLev2TexID);
+        uiWindow->Show(deactLev3TexID);
+        uiWindow->Show(deactLev4TexID);
+        uiWindow->Show(deactLev5TexID);
+        uiWindow->Show(deactLev6TexID);
+    }
+    if (unlockedLevels == 2) {
+        uiWindow->Show(lev1TexID);
+        uiWindow->Show(lev2TexID);
+        uiWindow->Hide(deactLev1TexID);
+        uiWindow->Hide(deactLev2TexID);
+        uiWindow->Show(deactLev3TexID);
+        uiWindow->Show(deactLev4TexID);
+        uiWindow->Show(deactLev5TexID);
+        uiWindow->Show(deactLev6TexID);
+    }
+    if (unlockedLevels == 3) {
+        uiWindow->Show(lev1TexID);
+        uiWindow->Show(lev2TexID);
+        uiWindow->Show(lev3TexID);
+        uiWindow->Hide(deactLev1TexID);
+        uiWindow->Hide(deactLev2TexID);
+        uiWindow->Hide(deactLev3TexID);
+        uiWindow->Show(deactLev4TexID);
+        uiWindow->Show(deactLev5TexID);
+        uiWindow->Show(deactLev6TexID);
+    }
+    if (unlockedLevels == 4) {
+        uiWindow->Show(lev1TexID);
+        uiWindow->Show(lev2TexID);
+        uiWindow->Show(lev3TexID);
+        uiWindow->Show(lev4TexID);
+        uiWindow->Hide(deactLev1TexID);
+        uiWindow->Hide(deactLev2TexID);
+        uiWindow->Hide(deactLev3TexID);
+        uiWindow->Hide(deactLev4TexID);
+        uiWindow->Show(deactLev5TexID);
+        uiWindow->Show(deactLev6TexID);
+    }
+    if (unlockedLevels == 5) {
+        uiWindow->Show(lev1TexID);
+        uiWindow->Show(lev2TexID);
+        uiWindow->Show(lev3TexID);
+        uiWindow->Show(lev4TexID);
+        uiWindow->Show(lev5TexID);
+        uiWindow->Hide(deactLev1TexID);
+        uiWindow->Hide(deactLev2TexID);
+        uiWindow->Hide(deactLev3TexID);
+        uiWindow->Hide(deactLev4TexID);
+        uiWindow->Hide(deactLev5TexID);
+        uiWindow->Show(deactLev6TexID);
+    }
+    if (unlockedLevels == 6) {
+        uiWindow->Show(lev1TexID);
+        uiWindow->Show(lev2TexID);
+        uiWindow->Show(lev3TexID);
+        uiWindow->Show(lev4TexID);
+        uiWindow->Show(lev5TexID);
+        uiWindow->Show(lev6TexID);
+        uiWindow->Hide(deactLev1TexID);
+        uiWindow->Hide(deactLev2TexID);
+        uiWindow->Hide(deactLev3TexID);
+        uiWindow->Hide(deactLev4TexID);
+        uiWindow->Hide(deactLev5TexID);
+        uiWindow->Hide(deactLev6TexID);
+    }
+}
+
+/* 4368C0-436C14 00354	*/
+void MenuOknoWyboru_CZ1::LoadUnlockedLevels() {
+    int local_a4 = 1; 
+    std::ifstream stream;
+    stream.open("data\\save\\nl.sav", std::ios::binary);
+    if (stream.fail()) {
+        return;
+    }
+    stream.read((char*) &local_a4, sizeof(int));
+    stream.close();
+
+    unlockedLevels = local_a4;
+    isIntroUnlocked = false;
+    isLevel1Unlocked = false;
+    isLevel2Unlocked = false;
+    isLevel3Unlocked = false;
+    isLevel4Unlocked = false;
+    isLevel5Unlocked = false;
+    isLevel6Unlocked = false;
+
+    if (unlockedLevels == 1) {
+        isIntroUnlocked = true;
+        isLevel1Unlocked = true;
+        isLevel2Unlocked = false;
+        isLevel3Unlocked = false;
+        isLevel4Unlocked = false;
+        isLevel5Unlocked = false;
+        isLevel6Unlocked = false;
+    }  
+    
+    if (unlockedLevels == 2) {
+        isIntroUnlocked = true;
+        isLevel1Unlocked = true;
+        isLevel2Unlocked = true;
+        isLevel3Unlocked = false;
+        isLevel4Unlocked = false;
+        isLevel5Unlocked = false;
+        isLevel6Unlocked = false;
+    }  
+    
+    if (unlockedLevels == 3) {
+        isIntroUnlocked = true;
+        isLevel1Unlocked = true;
+        isLevel2Unlocked = true;
+        isLevel3Unlocked = true;
+        isLevel4Unlocked = false;
+        isLevel5Unlocked = false;
+        isLevel6Unlocked = false;
+    }  
+    
+    if (unlockedLevels == 4) {
+        isIntroUnlocked = true;
+        isLevel1Unlocked = true;
+        isLevel2Unlocked = true;
+        isLevel3Unlocked = true;
+        isLevel4Unlocked = true;
+        isLevel5Unlocked = false;
+        isLevel6Unlocked = false;
+    }
+
+    if (unlockedLevels == 5) {
+        isIntroUnlocked = true;
+        isLevel1Unlocked = true;
+        isLevel2Unlocked = true;
+        isLevel3Unlocked = true;
+        isLevel4Unlocked = true;
+        isLevel5Unlocked = true;
+        isLevel6Unlocked = false;
+    } 
+
+    if (unlockedLevels == 6) {
+        isIntroUnlocked = true;
+        isLevel1Unlocked = true;
+        isLevel2Unlocked = true;
+        isLevel3Unlocked = true;
+        isLevel4Unlocked = true;
+        isLevel5Unlocked = true;
+        isLevel6Unlocked = true;
+    }
+
+}
