28ef64d439d94a9cea1f2dc04443a10f30d7abb8
Łukasz Starzyk <72223520+Aszman@users.noreply.github.com>
2026-09-14 19:30:12 +0200

    matched cMagFog (#28)

--- include/me2/cMagFog.h
+++ include/me2/cMagFog.h
@@ -3,24 +3,33 @@
 
 #include <globals.h>
 
-#include "d3d8.h"
+#include "cMagUtility.h"
 
-class cMagFog {
+class DLLEXPORT cMagFog : public cMagUtility {
 public:
-	/* 10022D00 */ cMagFog(cMagFog* param_1);
-	/* 10022E20 */ cMagFog* operator=(cMagFog* param_1);
-	/* 10022F30 */ cMagFog* scalar_destructor(uchar param_1);
+	/* 1005A5E0 */ void InitDeviceObjects(cMagGameObject* param_1);
 	/* 10072470 */ cMagFog();
 	/* 10072500 */ virtual ~cMagFog();
 	/* 10072560 */ void TypeFog(ulong param_1);
 	/* 10072570 */ void InitFog();
 	/* 10072610 */ void SetFogRangeNear(float param_1);
+	/* 10072620 */ void SetFogRangeFar(float param_1);
+	/* 10072630 */ void SetFogDensity(float param_1);
 	/* 10072640 */ void SetColorFog(uchar param_1, uchar param_2, uchar param_3);
 	/* 10072680 */ void EnableFog();
 	/* 100726B0 */ void DisableFog();
-	/* 100726E0 */ void SetColorFog(ulong param_1);
+	/* 100726E0 */ void SetColorFog(D3DCOLOR param_1);
 	/* 100726F0 */ D3DCOLOR GetColorFog();
 	/* 10072700 */ void Enable(bool param_1);
+
+private:
+	/* 0xd4c */ float fogRangeNear;
+	/* 0xd50 */ float fogRangeFar;
+	/* 0xd54 */ float fogDensity;
+	/* 0xd58 */ D3DCOLOR colorFog;
+	/* 0xd5c */ D3DFOGMODE typeFog;
 };
+
+STATIC_ASSERT(sizeof(cMagFog) == 0xd60);
 
 #endif
