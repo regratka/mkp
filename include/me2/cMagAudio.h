2c785f3a046dfb90b5769bec106a583a9f6ae780
Łukasz Starzyk <72223520+Aszman@users.noreply.github.com>
2026-09-15 20:15:55 +0200

    matched me2/cMagAudio (#30)

--- include/me2/cMagAudio.h
+++ include/me2/cMagAudio.h
@@ -2,16 +2,26 @@
 #define C_MAG_AUDIO
 
 #include <globals.h>
+#include <Dmusici.h>
 
-class cMagAudio {
+#include "cMagLog.h"
+
+
+class DLLEXPORT cMagAudio {
 public:
-	/* 10001ED0 */ cMagAudio(cMagAudio* param_1);
-	/* 10001F00 */ cMagAudio* operator=(cMagAudio* param_1);
-	/* 10001F30 */ cMagAudio* scalar_destructor(uchar param_1);
+	/* 1000B510 */ IDirectMusicPerformance8* getPerformance();
+	/* 1000B520 */ IDirectMusicLoader8* getLoader();
 	/* 100884A0 */ cMagAudio();
-	/* 100884C0 */ ~cMagAudio();
-	/* 100884D0 */ long Setup();
+	/* 100884C0 */ virtual ~cMagAudio();
+	/* 100884D0 */ HRESULT Setup();
 	/* 10088540 */ void Kill();
+
+private:
+	/* 0x04 */ IDirectMusicPerformance8* musicPerformance;
+	/* 0x08 */ IDirectMusicLoader8* musicLoader;
+	/* 0x0c */ cMagLog log;
 };
+
+STATIC_ASSERT(sizeof(cMagAudio) == 0x14);
 
 #endif
