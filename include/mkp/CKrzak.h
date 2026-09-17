bfe00be0fba970af4b3924ec8b90455e1fdd5f2e
Aszman <lukaszstarzyk1@gmail.com>
2026-09-17 16:41:45 +0200

    matched CKrzak

--- include/mkp/CKrzak.h
+++ include/mkp/CKrzak.h
@@ -2,17 +2,33 @@
 #define _C_KRZAK
 
 #include <globals.h>
+#include "cMagMeshObject.h"
 
-class CKrzak {
+struct CKrzakChunkData {
+	/* 0x00 */ float unk_00;
+	/* 0x04 */ float unk_04;
+};
+
+class CKrzak : public cMagMeshObject {
 public:
 	/* 441A50 */ CKrzak();
-	/* 441A90 */ CKrzak* scalar_destructor(uchar param_1);
-	/* 441AB0 */ ~CKrzak();
+	/* 441AB0 */ virtual ~CKrzak();
 	/* 441AC0 */ void* EXP();
 	/* 441B20 */ void OnActivate();
 	/* 441B60 */ void OnActivateLevel();
 	/* 441BB0 */ void OnFrame();
-	/* 441C80 */ uchar OnLoadChunk(uint param_1, int param_2, uint param_3, void* param_4);
+	/* 441C80 */ void OnLoadChunk(_ED_CHUNK param_1, std::ifstream& param_2);
+
+private:
+	/* 0x2658 */ CKrzakChunkData unk_2658;
+	// /* 0x265c */ float unk_265c;
+	/* 0x2660 */ float unk_2660;
+	/* 0x2664 */ float unk_2664;
+	/* 0x2668 */ bool unk_2668;
+	/* 0x266c */ float unk_266c;
+	/* 0x2670 */ float unk_2670;
 };
+
+STATIC_ASSERT(sizeof(CKrzak) == 0x2678);
 
 #endif
--- src/mkp/CKrzak.cpp
+++ src/mkp/CKrzak.cpp
@@ -2,11 +2,9 @@
 
 /* 441A50-441A84 00034	*/
 CKrzak::CKrzak() {
-}
-
-/* 441A90-441AAE 0001E	*/
-CKrzak* CKrzak::scalar_destructor(uchar param_1) {
-	return 0;
+	ZeroMemory(&unk_2658, sizeof(CKrzakChunkData));
+	unk_266c = 0.01f;
+	unk_2670 = 0.05f;
 }
 
 /* 441AB0-441ABC 0000C	*/
@@ -15,23 +13,56 @@ CKrzak::~CKrzak() {
 
 /* 441AC0-441B1A 0005A	*/
 void* CKrzak::EXP() {
-	return 0;
+	return new CKrzak();
 }
 
 /* 441B20-441B51 00031	*/
 void CKrzak::OnActivate() {
+	unk_2660 = GetTickCount() / 1000.0f;
+	unk_2664 = 0.0f;
 }
 
 /* 441B60-441BAC 0004C	*/
 void CKrzak::OnActivateLevel() {
+	if (unk_2658.unk_00 > 0.0f) {
+		unk_266c = unk_2658.unk_00;
+	}
+	if (unk_2658.unk_04 > 0.0f) {
+		unk_2670 = unk_2658.unk_04;
+	}
+
+	EnableCallHandler("OnFrame");
 }
 
 /* 441BB0-441C77 000C7	*/
 void CKrzak::OnFrame() {
+	float DVar1 = GetTickCount() / 1000.0f;
+	if (unk_266c >= DVar1 - unk_2660) {
+		return;
+	}
+	unk_2660 = DVar1;
+
+	if (!unk_2668) {
+		unk_2664 += unk_2670;
+	} 
+	if (unk_2668) {
+		unk_2664 -= unk_2670;
+	}
+
+	SetDirection(unk_2664, 0.0f, unk_2664);
+
+	if (unk_2664 > 3.0f) {
+		unk_2668 = true;
+	} 
+	if (unk_2664 < -3.0f) {
+		unk_2668 = false;
+	}
 }
 
 /* 441C80-441CA4 00024	*/
-uchar CKrzak::OnLoadChunk(uint param_1, int param_2, uint param_3, void* param_4) {
-	return 0;
+void CKrzak::OnLoadChunk(_ED_CHUNK param_1, std::ifstream& param_2) {
+	if (param_1.chunkType == 0xf && param_1.chunkDataSize == 8) {
+		param_2.read((char*)&unk_2658, sizeof(CKrzakChunkData));
+	}
 }
 
