62dc76250feb4f4ef5593379a12be2fa75ade8ba
Aszman <lukaszstarzyk1@gmail.com>
2026-09-17 22:26:11 +0200

    matched cFadeInOut

--- include/mkp/cFadeInOut.h
+++ include/mkp/cFadeInOut.h
@@ -7,8 +7,7 @@
 class cFadeInOut : public cMagSprite {
 public:
 	/* 41C4A0 */ cFadeInOut();
-	/* 41C530 */ cMagSprite* scalar_destructor(uchar param_1);
-	/* 41C550 */ ~cFadeInOut();
+	/* 41C550 */ virtual ~cFadeInOut();
 	/* 41C560 */ void FUN0041c560();
 	/* 41C5A0 */ void OnActivate();
 	/* 41C5E0 */ void FUN0041c5e0(cMagKernel* param_1);
@@ -18,7 +17,20 @@ public:
 	/* 41C830 */ void FUN0041c830(bool param_1);
 
 private:
-	/* 0x1740 */ uchar field_0x1740[0x1770-0x1740];
+	/* 0x1740 */ int unk_1740;
+	/* 0x1744 */ float unk_1744;
+	/* 0x1748 */ uchar field_0x1748[0x174c-0x1748];
+	/* 0x174c */ bool unk_174c;
+	/* 0x174d */ bool unk_174d;
+	/* 0x174e */ bool unk_174e;
+	/* 0x174f */ bool unk_174f;
+	/* 0x1750 */ cMagKernel* unk_1750;
+	/* 0x1754 */ uchar field_0x1754[0x175c-0x1754];
+	/* 0x175c */ float unk_175c;
+	/* 0x1760 */ float unk_1760;
+	/* 0x1764 */ int unk_1764;
+	/* 0x1768 */ bool unk_1768;
+	/* 0x1769 */ uchar field_0x1769[0x1770-0x1769];
 };
 
 STATIC_ASSERT(sizeof(cFadeInOut) == 0x1770);
--- src/mkp/cFadeInOut.cpp
+++ src/mkp/cFadeInOut.cpp
@@ -1,12 +1,19 @@
 #include "cFadeInOut.h"
 
+#include "CGame.h"
+
 /* 41C4A0-41C52A 0008A	*/
 cFadeInOut::cFadeInOut() {
-}
-
-/* 41C530-41C54E 0001E	*/
-cMagSprite* cFadeInOut::scalar_destructor(uchar param_1) {
-	return 0;
+	SetObjectName("cFadeInOut");
+	unk_174c = false;
+	unk_174d = false;
+	unk_174e = false;
+	unk_174f = false;
+	unk_1750 = NULL;
+	unk_1760 = 0.0f;
+	unk_175c = 1.0f;
+	unk_1764 = 0;
+	unk_1768 = false;
 }
 
 /* 41C550-41C55C 0000C	*/
@@ -15,29 +22,109 @@ cFadeInOut::~cFadeInOut() {
 
 /* 41C560-41C596 00036	*/
 void cFadeInOut::FUN0041c560() {
+	if (!unk_1768) {
+		Hide(unk_1740);
+	} else {
+		Hide(unk_1764);
+	}
+	DisableCallHandler("OnFrame");
 }
 
 /* 41C5A0-41C5DB 0003B	*/
 void cFadeInOut::OnActivate() {
+	unk_1740 = AddTexture("data\\textures\\BlackFade.png");
+	SetScaleAsWindow(unk_1740);
+	unk_1764 = AddTexture("data\\textures\\BlackFade.png");
+	SetScaleAsWindow(unk_1764);
 }
 
 /* 41C5E0-41C5FA 0001A	*/
 void cFadeInOut::FUN0041c5e0(cMagKernel* param_1) {
+	if (param_1 == NULL) {
+		unk_1750 = NULL;
+	} else {
+		unk_1750 = param_1;
+	}
 }
 
 /* 41C600-41C743 00143	*/
 void cFadeInOut::OnFrame() {
+	float fVar1 = GetGame()->GetFrameTime();
+	unk_1760 = fVar1 / (1.0f / unk_175c);
+	if (unk_1760 > 1.0f) {
+		return;
+	}
+
+	if (unk_174c) {
+		unk_1744 -= unk_1760;
+		if (unk_1744 <= 0.0f) {
+			unk_174f = true;
+			if (unk_1750 != NULL) {
+				unk_1750->OnFadeInEnd();
+			}
+
+			DisableCallHandler("OnFrame");
+			if (!unk_1768) {
+				Show(unk_1740);
+			} else {
+				Show(unk_1764);
+			}
+		}
+	}
+
+	if (unk_174d) {
+		unk_1744 += unk_1760;
+		if (unk_1744 >= 1.0f) {
+			unk_174e = true;
+			DisableCallHandler("OnFrame");
+			if (unk_1750 != NULL) {
+				unk_1750->OnFadeOutEnd();
+			}
+		}
+	}
+
+	if (!unk_1768) {
+		SetAlpha(unk_1740, unk_1744);
+	} else {
+		SetAlpha(unk_1764, unk_1744);
+	}
+	
 }
 
 /* 41C750-41C7AB 0005B	*/
 void cFadeInOut::FUN0041c750() {
+	unk_174c = true;
+	unk_174d = false;
+	unk_174e = false;
+	unk_174f = false;
+	unk_1744 = 1.0f;
+	if (!unk_1768) {
+		Show(unk_1740);
+	} else {
+		Show(unk_1764);
+	}
+	EnableCallHandler("OnFrame");
 }
 
 /* 41C7B0-41C826 00076	*/
 void cFadeInOut::FUN0041c7b0() {
+	unk_174d = true;
+	unk_174c = false;
+	unk_174e = false;
+	unk_174f = false;
+	unk_1744 = 0.0f;
+	if (!unk_1768) {
+		SetAlpha(unk_1740, 0.0f);
+		Show(unk_1740);
+	} else {
+		SetAlpha(unk_1764, 0.0f);
+		Show(unk_1764);
+	}
+	EnableCallHandler("OnFrame");
 }
 
 /* 41C830-41C83D 0000D	*/
 void cFadeInOut::FUN0041c830(bool param_1) {
+	unk_1768 = param_1;
 }
 
