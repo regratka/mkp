baa042e8918ebea9ec4612877274f94a0de90e7e
Łukasz Starzyk <72223520+Aszman@users.noreply.github.com>
2026-09-17 16:51:14 +0200

    matched mkp/CKrzak (#32)
    * matched CKrzak
    
    * renamed attributes

--- include/mkp/CKrzak.h
+++ include/mkp/CKrzak.h
@@ -2,17 +2,32 @@
 #define _C_KRZAK
 
 #include <globals.h>
+#include "cMagMeshObject.h"
 
-class CKrzak {
+struct CKrzakChunkData {
+	/* 0x00 */ float directionUpdateInterval;
+	/* 0x04 */ float directionChangeValue;
+};
+
+class CKrzak : public cMagMeshObject {
 public:
 	/* 441A50 */ CKrzak();
-	/* 441A90 */ CKrzak* scalar_destructor(uchar param_1);
-	/* 441AB0 */ ~CKrzak();
+	/* 441AB0 */ virtual ~CKrzak();
 	/* 441AC0 */ void* EXP();
 	/* 441B20 */ void OnActivate();
 	/* 441B60 */ void OnActivateLevel();
 	/* 441BB0 */ void OnFrame();
-	/* 441C80 */ uchar OnLoadChunk(uint param_1, int param_2, uint param_3, void* param_4);
+	/* 441C80 */ void OnLoadChunk(_ED_CHUNK param_1, std::ifstream& param_2);
+
+private:
+	/* 0x2658 */ CKrzakChunkData chunkData;
+	/* 0x2660 */ float lastUpdateTick;
+	/* 0x2664 */ float direction;
+	/* 0x2668 */ bool shouldDecreaseDirection;
+	/* 0x266c */ float directionUpdateInterval;
+	/* 0x2670 */ float directionChangeValue;
 };
+
+STATIC_ASSERT(sizeof(CKrzak) == 0x2678);
 
 #endif
--- src/mkp/CKrzak.cpp
+++ src/mkp/CKrzak.cpp
@@ -2,11 +2,9 @@
 
 /* 441A50-441A84 00034	*/
 CKrzak::CKrzak() {
-}
-
-/* 441A90-441AAE 0001E	*/
-CKrzak* CKrzak::scalar_destructor(uchar param_1) {
-	return 0;
+	ZeroMemory(&chunkData, sizeof(CKrzakChunkData));
+	directionUpdateInterval = 0.01f;
+	directionChangeValue = 0.05f;
 }
 
 /* 441AB0-441ABC 0000C	*/
@@ -15,23 +13,56 @@ CKrzak::~CKrzak() {
 
 /* 441AC0-441B1A 0005A	*/
 void* CKrzak::EXP() {
-	return 0;
+	return new CKrzak();
 }
 
 /* 441B20-441B51 00031	*/
 void CKrzak::OnActivate() {
+	lastUpdateTick = GetTickCount() / 1000.0f;
+	direction = 0.0f;
 }
 
 /* 441B60-441BAC 0004C	*/
 void CKrzak::OnActivateLevel() {
+	if (chunkData.directionUpdateInterval > 0.0f) {
+		directionUpdateInterval = chunkData.directionUpdateInterval;
+	}
+	if (chunkData.directionChangeValue > 0.0f) {
+		directionChangeValue = chunkData.directionChangeValue;
+	}
+
+	EnableCallHandler("OnFrame");
 }
 
 /* 441BB0-441C77 000C7	*/
 void CKrzak::OnFrame() {
+	float DVar1 = GetTickCount() / 1000.0f;
+	if (directionUpdateInterval >= DVar1 - lastUpdateTick) {
+		return;
+	}
+	lastUpdateTick = DVar1;
+
+	if (!shouldDecreaseDirection) {
+		direction += directionChangeValue;
+	} 
+	if (shouldDecreaseDirection) {
+		direction -= directionChangeValue;
+	}
+
+	SetDirection(direction, 0.0f, direction);
+
+	if (direction > 3.0f) {
+		shouldDecreaseDirection = true;
+	} 
+	if (direction < -3.0f) {
+		shouldDecreaseDirection = false;
+	}
 }
 
 /* 441C80-441CA4 00024	*/
-uchar CKrzak::OnLoadChunk(uint param_1, int param_2, uint param_3, void* param_4) {
-	return 0;
+void CKrzak::OnLoadChunk(_ED_CHUNK param_1, std::ifstream& param_2) {
+	if (param_1.chunkType == 0xf && param_1.chunkDataSize == 8) {
+		param_2.read((char*)&chunkData, sizeof(CKrzakChunkData));
+	}
 }
 
